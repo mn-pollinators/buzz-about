5df643a8e4be66b149c9709f0566c4f90b0afabe
michael-small <small203@morris.umn.edu>
2019-07-05 15:07:40 -0500

    Read properties of a-images from JSON

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Pipe, PipeTransform } from '@angular/core';
+import JSON from 'src/assets/roundtest.json';
 
 @Component({
   selector: 'app-root',
@@ -11,8 +12,27 @@ export class AppComponent {
 
   collapseBar: boolean = false;
 
+  constructor() {
+    console.log('Reading local json files');
+    console.log(JSON);
+   }
+
   atts = {
     'height': 3,
     'imgs': ['apis.png','flower.png']
   }
+
+  ngAfterViewInit(): void { //Thanks to HeinPauwelyn @ https://github.com/aframevr/aframe/issues/2518#issuecomment-289450266
+
+    for (let i: number = JSON.markers.length; i--;) {
+
+        let image: any = JSON.markers[i],
+            el: any = document.getElementById(image.id);
+
+        el.setAttribute("position", image.position);
+        el.setAttribute("src", image.src);
+        el.setAttribute("rotation", image.rotation);
+        el.setAttribute("scale", image.scale);
+    }
+}
 }
