1f3c1623b658f3fb46ede2d8e8f004ccf95f5efb
michael-small <small203@morris.umn.edu>
2019-07-12 16:30:34 -0500

    Add delay to app.component showing
    See comments. Attribute setting function is broken for now due to this delay but this is likely manageable.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,17 +10,29 @@ import JSON from 'src/assets/roundtest.json';
 export class AppComponent {
   title = 'ar-simulation';
 
+  public showContent: boolean = false;
+
   ngAfterViewInit(): void { //Thanks to HeinPauwelyn @ https://github.com/aframevr/aframe/issues/2518#issuecomment-289450266
 
-    for (let i: number = JSON.markers.length; i--;) {
+    // TODO: This is broken for now until we figure out how to relate the timeout to the assignment
 
-        let image: any = JSON.markers[i],
-            el: any = document.getElementById(image.id);
+    // for (let i: number = JSON.markers.length; i--;) {
 
-        el.setAttribute("position", image.position);
-        el.setAttribute("src", image.src);
-        el.setAttribute("rotation", image.rotation);
-        el.setAttribute("scale", image.scale);
-    }
+    //     let image: any = JSON.markers[i],
+    //         el: any = document.getElementById(image.id);
+
+    //     el.setAttribute("position", image.position);
+    //     el.setAttribute("src", image.src);
+    //     el.setAttribute("rotation", image.rotation);
+    //     el.setAttribute("scale", image.scale);
+    // }
+  }
+
+  public ngOnInit() {
+    setTimeout(()=>this.showContent=true, 1000); 
+    //Thanks to Valikhan Akhmedov, Oct 4 '16 at 12:22 @ https://stackoverflow.com/a/39852249/8273171
+    //This move was inspired by kawalt's react-aframe-ar-test's index.js @ https://github.com/kalwalt/react-aframe-ar-test/blob/master/src/index.js
+    //which was inspired by fega @ https://github.com/jeromeetienne/AR.js/issues/493#issuecomment-490303718
   }
 }
+// }
