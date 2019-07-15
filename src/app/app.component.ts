a40e618fe3e39d9e696da6d3939bd8f9263247ab
Utkarsh Kumar <kumar375@morris.umn.edu>
2019-07-15 18:54:21 -0500

    Add event listener to wait for DOM
    Adding a event listener that waits until the DOM has fully loaded the problem of not finding any elements due to ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,43 +1,41 @@
-import { Component,  NgZone } from '@angular/core';
-import { Pipe, PipeTransform } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import JSON from 'src/assets/roundtest.json';
+import { DynamicScriptLoaderService } from 'src/app/DynamicScriptLoaderService'  
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'ar-simulation';
 
   JSON: any;
-
   public showContent: boolean = false;
 
-  public zone;
+  constructor(private dynamicScriptLoader: DynamicScriptLoaderService) {}
 
-  constructor(zone: NgZone) {
-    this.zone = zone;
+  ngOnInit() {
+    this.loadScripts();
+
+    window.addEventListener('load', (event) => {  //Thanks to this article: https://www.kirupa.com/html5/running_your_code_at_the_right_time.htm
+      
+      for (let i: number = JSON.markers.length; i--;) { //Thanks to HeinPauwelyn @ https://github.com/aframevr/aframe/issues/2518#issuecomment-289450266
+        let image: any = JSON.markers[i],
+        el: any = document.getElementById(image.id); 
+        el.setAttribute("position", image.position); 
+        el.setAttribute("src", image.src);
+        el.setAttribute("rotation", image.rotation);
+        el.setAttribute("scale", image.scale);
+      }
+    });
   }
 
-  public ngOnInit() {
-    setTimeout(()=>this.showContent=true, 0.1); 
-    this.zone.runOutsideAngular(() => {
-      setInterval(() => {
-        for (let i: number = JSON.markers.length; i--;) { //Thanks to HeinPauwelyn @ https://github.com/aframevr/aframe/issues/2518#issuecomment-289450266
-
-          let image: any = JSON.markers[i],
-              el: any = document.getElementById(image.id);
-  
-          el.setAttribute("position", image.position);
-          el.setAttribute("src", image.src);
-          el.setAttribute("rotation", image.rotation);
-          el.setAttribute("scale", image.scale);
-        }
-      }, 0.1);
-    })
-    //Thanks to Valikhan Akhmedov, Oct 4 '16 at 12:22 @ https://stackoverflow.com/a/39852249/8273171
-    //This move was inspired by kawalt's react-aframe-ar-test's index.js @ https://github.com/kalwalt/react-aframe-ar-test/blob/master/src/index.js
-    //which was inspired by fega @ https://github.com/jeromeetienne/AR.js/issues/493#issuecomment-490303718
+   
+  private loadScripts() {
+    this.dynamicScriptLoader.load('aframe','arjs').then(data => {
+      this.showContent=true;
+    }).catch(error => console.log(error));
   }
 }
