e4c664e9e9086f1ae237b24c4124602fbb679bcb
Paul Friederichsen <floogulinc@gmail.com>
2019-07-16 03:40:32 -0500

    Test adding markers through the input to the component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,11 @@ export class AppComponent {
 
   collapseBar: boolean = false;
 
+  markers = [
+    {id:"1", barcodeValue:1, imgPath:"assets/icons/icon-512x512.png"},
+    {id:"2", barcodeValue:2, imgPath:"assets/icons/icon-512x512.png"},
+    {id:"3", barcodeValue:3, imgPath:"assets/icons/icon-512x512.png"},
+    {id:"4", barcodeValue:4, imgPath:"assets/icons/icon-512x512.png"}
+  ]
+
 }
