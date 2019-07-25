fc28a0f21145c128b55ed487626ae839dcd47534
Matt <mmunns16@gmail.com>
2019-07-25 15:16:46 -0500

    Make large-display height adjust to window size This allows the background to be seen properly

--- src/app/large-display/large-display.component.ts
+++ src/app/large-display/large-display.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, OnInit, ViewChild } from '@angular/core';
+import { Component, Inject, OnInit, ViewChild, HostListener } from '@angular/core';
 import { TimerBarComponent } from 'src/app/timer-bar/timer-bar.component'
 
 @Component({
@@ -8,13 +8,21 @@ import { TimerBarComponent } from 'src/app/timer-bar/timer-bar.component'
 })
 export class LargeDisplayComponent implements OnInit {
 
+  componentHeight: number;
+
   @ViewChild('timerBar', {static: true}) timerBar: TimerBarComponent;
-  constructor() {   
-    
+
+  constructor() { }
+
+  // resize the component height with the window
+  @HostListener('window:resize', ['$event']) onResize(event) {
+    this.componentHeight = window.innerHeight-83;
   }
 
   ngOnInit() {
     this.timerBar.startTimer(60);
+    // set the height of the component so that the background actually shows up. Subtract 83 to account for the navbar and whitespace
+    this.componentHeight = window.innerHeight-83;
   }
 
 }
