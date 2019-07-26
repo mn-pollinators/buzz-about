c111b65af7942cd7b9c70d42fd651566716584c5
Utkarsh Kumar <kumar375@morris.umn.edu>
2019-07-26 11:53:56 -0500

    Modify path to Ar-View
    Currently, clicking on "I am a teacher" or "I am a student" leads to the PlayRoundComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PlayRoundComponent } from './play-round/play-round.component';
+import { EntryScreenComponent } from './entry-screen/entry-screen.component';
 
 const routes: Routes = [
-  {path: "", component: PlayRoundComponent}
+  {path: "", component: EntryScreenComponent},
+  {path:"ar", component: PlayRoundComponent}
 ];
 
 @NgModule({
