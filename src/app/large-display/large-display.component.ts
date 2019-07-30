be25ebb9e511464efad2f40a049a2098d4d54293
michael-small <small203@morris.umn.edu>
2019-07-30 12:08:27 -0500

    Add red dot for debugging highest point to place flower roots
    updateFlowerRootHighestPoint() once debugged should be able to find a fixed point on the teacher's prairie background. This can be used to set the highest point that the flower grid can place flowers, particularly the "roots" of the flower's. Once we think that it is accurate, we can remove the pointer and use the Y value given for flower grid placement.

--- src/app/large-display/large-display.component.ts
+++ src/app/large-display/large-display.component.ts
@@ -9,6 +9,21 @@ import { TimerBarComponent } from 'src/app/timer-bar/timer-bar.component'
 export class LargeDisplayComponent implements OnInit {
 
   componentHeight: number;
+  componentWidth: number;
+
+  // Fixed point on background image variables
+  // Daniel Imms answered Apr 5 '13 at 15:43 @ https://stackoverflow.com/a/15838104
+  image = { width: 1920, height: 1224 }; //cropped field background-image 
+  target = { x: 0, y: 608 }; //the lowest point of the field in which a flower's "root" could be placed, aka the highest place the bottom of a flower could be
+  windowWidth: number;
+  windowHeight: number;
+  xScale: number;
+  yScale: number; 
+  scale: number;
+  yOffset: number;
+  xOffset: number;
+  topPosition: number;
+  leftPosition: number;
 
   @ViewChild('timerBar', {static: true}) timerBar: TimerBarComponent;
 
@@ -17,12 +32,45 @@ export class LargeDisplayComponent implements OnInit {
   // resize the component height with the window
   @HostListener('window:resize', ['$event']) onResize(event) {
     this.componentHeight = window.innerHeight-83;
+    this.componentWidth = window.innerWidth;
+    console.log("Window.innerHeight: " + window.innerHeight);
+    this.updateFlowerRootHighestPoint();
   }
 
   ngOnInit() {
     this.timerBar.startTimer(60);
     // set the height of the component so that the background actually shows up. Subtract 83 to account for the navbar and whitespace
     this.componentHeight = window.innerHeight-83;
+    this.componentWidth = window.innerWidth;
+
+    this.updateFlowerRootHighestPoint();
   }
 
+  private updateFlowerRootHighestPoint() { // Daniel Imms answered Apr 5 '13 at 15:43 @ https://stackoverflow.com/a/15838104
+    this.windowWidth = this.componentWidth;
+    this.windowHeight = this.componentHeight;
+
+    // Get largest dimension increase
+    this.xScale = this.windowWidth / this.image.width;
+    this.yScale = this.windowHeight / this.image.height;
+    this.scale;
+    this.yOffset = 0;
+    this.xOffset = 0;
+
+    if (this.xScale > this.yScale) {
+      // The image fits perfectly in x axis, stretched in y
+      this.scale = this.xScale;
+      this.yOffset = (this.windowHeight - (this.image.height * this.scale)) / 2;
+    } else {
+      // The image fits perfectly in y axis, stretched in x
+      this.scale = this.yScale;
+      this.xOffset = (this.windowWidth - (this.image.width * this.scale)) / 2;
+    }
+
+    console.log("THE COMPONENTHEIGHT IS: " + this.componentHeight);
+    this.topPosition = (this.target.y) * this.scale + this.yOffset;
+    this.leftPosition = (this.target.x) * this.scale + this.xOffset;
+    
+    console.log("topPosition: " + this.topPosition + "... leftPosition: " + this.leftPosition);
+  }
 }
