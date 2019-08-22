719c69b3bb8b95b742f854c1b55f2f9cdc0dd7ca
Utkarsh Kumar <kumar375@morris.umn.edu>
2019-08-22 12:41:31 -0500

    Entry Screen In progress

--- src/app/entry-screen/entry-screen.component.ts
+++ src/app/entry-screen/entry-screen.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-entry-screen',
   templateUrl: './entry-screen.component.html',
   styleUrls: ['./entry-screen.component.scss']
 })
+
 export class EntryScreenComponent implements OnInit {
 
-  constructor() { }
+  componentHeight: number;
 
-  ngOnInit() {
+  @HostListener('window:resize', ['$event']) onResize(event) {
+    this.componentHeight = window.innerHeight-80;
   }
 
+  ngOnInit() {
+    this.componentHeight = window.innerHeight-80;
+  }
 }
