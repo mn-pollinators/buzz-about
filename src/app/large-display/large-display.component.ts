d1d0dae71bfc4a3e35bd13196fe2a17a606ff22a
Waller Li <lsw626313@gmail.com>
2020-02-24 11:54:05 -0600

    Format code and fix TSlint warnings

--- src/app/large-display/large-display.component.ts
+++ src/app/large-display/large-display.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, HostListener } from '@angular/core';
+import { Component, OnInit, AfterViewInit, HostListener } from '@angular/core';
 import { Flower } from 'src/app/item';
 
 @Component({
@@ -6,14 +6,15 @@ import { Flower } from 'src/app/item';
   templateUrl: './large-display.component.html',
   styleUrls: ['./large-display.component.scss']
 })
-export class LargeDisplayComponent implements OnInit {
+
+export class LargeDisplayComponent implements OnInit, AfterViewInit {
 
   componentHeight: number;
   componentWidth: number;
 
-  flowers: Flower[] = new Array;
-  flowers_A: Flower[] = new Array;
-  flowers_B: Flower[] = new Array;
+  flowers: Flower[] = new Array<Flower>();
+  flowersA: Flower[] = new Array<Flower>();
+  flowersB: Flower[] = new Array<Flower>();
 
   constructor() { }
 
@@ -24,12 +25,12 @@ export class LargeDisplayComponent implements OnInit {
       f.y = f.y / this.componentWidth * window.innerWidth;
       // f.y = f.y / this.componentHeight * window.innerHeight;
     }
-    for (const f of this.flowers_A) {
+    for (const f of this.flowersA) {
       f.x = f.x / this.componentWidth * window.innerWidth;
       f.y = f.y / this.componentWidth * window.innerWidth;
       // f.y = f.y / this.componentHeight * window.innerHeight;
     }
-    for (const f of this.flowers_B) {
+    for (const f of this.flowersB) {
       f.x = f.x / this.componentWidth * window.innerWidth;
       f.y = f.y / this.componentWidth * window.innerWidth;
       // f.y = f.y / this.componentHeight * window.innerHeight;
@@ -56,38 +57,72 @@ export class LargeDisplayComponent implements OnInit {
 
   initializeTestFlowers() {
 
-    this.flowers.push(new Flower('bee_a', 'assets/images/1000w-8bit/bees/rusty patch bumblebee.png', 0.5*this.componentWidth, 0.5*this.componentHeight, 6, false));
-    this.flowers.push(new Flower('bee_b', 'assets/images/1000w-8bit/bees/megachile pugnata.png', 0.5*this.componentWidth, 0.5*this.componentHeight, 6, false));
-    this.flowers.push(new Flower('bee_c', 'assets/images/1000w-8bit/bees/colletes simulans.png', 0.5*this.componentWidth, 0.5*this.componentHeight, 7, false));
-    this.flowers.push(new Flower('a',  'assets/images/1000w-8bit/flowers/rudbeckia hirta.png', 0.5*this.componentWidth, 0.5*this.componentHeight,25, false));
-    this.flowers.push(new Flower('b', 'assets/images/1000w-8bit/flowers/taraxacum officinale.png', 0.5*this.componentWidth, 0.5*this.componentHeight, 14, false));
-    this.flowers.push(new Flower('c', 'assets/images/1000w-8bit/flowers/solidago rigida.png', 0.5*this.componentWidth, 0.5*this.componentHeight, 18, false));
-    this.flowers.push(new Flower('d', 'assets/images/1000w-8bit/flowers/sunflower.png', 0.5*this.componentWidth, 0.5*this.componentHeight, 14, false));
-    this.flowers.push(new Flower('e', 'assets/images/1000w-8bit/flowers/black raspberry.png', 0.5*this.componentWidth, 0.5*this.componentHeight, 12, false));
-    this.flowers.push(new Flower('f', 'assets/images/1000w-8bit/flowers/trifolium repens.png', 0.5*this.componentWidth, 0.5*this.componentHeight, 14, false));
-    this.flowers.push(new Flower('g', 'assets/images/1000w-8bit/flowers/vaccinium angustifolium.png', 0.5*this.componentWidth, 0.5*this.componentHeight, 22, false));
+    this.flowers.push(new Flower(
+      'bee_a', 'assets/images/1000w-8bit/bees/rusty patch bumblebee.png', 0.5 * this.componentWidth, 0.5 * this.componentHeight, 6, false));
+    this.flowers.push(new Flower(
+      'bee_b', 'assets/images/1000w-8bit/bees/megachile pugnata.png', 0.5 * this.componentWidth, 0.5 * this.componentHeight, 6, false));
+    this.flowers.push(new Flower(
+      'bee_c', 'assets/images/1000w-8bit/bees/colletes simulans.png', 0.5 * this.componentWidth, 0.5 * this.componentHeight, 7, false));
+    this.flowers.push(new Flower(
+      'a',  'assets/images/1000w-8bit/flowers/rudbeckia hirta.png', 0.5 * this.componentWidth, 0.5 * this.componentHeight, 25, false));
+    this.flowers.push(new Flower(
+      'b', 'assets/images/1000w-8bit/flowers/taraxacum officinale.png', 0.5 * this.componentWidth, 0.5 * this.componentHeight, 14, false));
+    this.flowers.push(new Flower(
+      'c', 'assets/images/1000w-8bit/flowers/solidago rigida.png', 0.5 * this.componentWidth, 0.5 * this.componentHeight, 18, false));
+    this.flowers.push(new Flower(
+      'd', 'assets/images/1000w-8bit/flowers/sunflower.png', 0.5 * this.componentWidth, 0.5 * this.componentHeight, 14, false));
+    this.flowers.push(new Flower(
+      'e', 'assets/images/1000w-8bit/flowers/black raspberry.png', 0.5 * this.componentWidth, 0.5 * this.componentHeight, 12, false));
+    this.flowers.push(new Flower(
+      'f', 'assets/images/1000w-8bit/flowers/trifolium repens.png', 0.5 * this.componentWidth, 0.5 * this.componentHeight, 14, false));
+    this.flowers.push(new Flower(
+      'g', 'assets/images/1000w-8bit/flowers/vaccinium angustifolium.png', 0.5 * this.componentWidth, 0.5 * this.componentHeight, 22,
+      false));
 
-    this.flowers_A.push(new Flower('bee_a', 'assets/images/1000w-8bit/bees/rusty patch bumblebee.png', 0.5*this.componentWidth, 0.5*this.componentHeight, 6, false));
-    this.flowers_A.push(new Flower('bee_b', 'assets/images/1000w-8bit/bees/megachile pugnata.png', 0.5*this.componentWidth, 0.5*this.componentHeight, 6, false));
-    this.flowers_A.push(new Flower('bee_c', 'assets/images/1000w-8bit/bees/colletes simulans.png', 0.5*this.componentWidth, 0.5*this.componentHeight, 7, false));
-    this.flowers_A.push(new Flower('a',  'assets/images/1000w-8bit/flowers/rudbeckia hirta.png', 0.5*this.componentWidth, 0.5*this.componentHeight,25, false));
-    this.flowers_A.push(new Flower('b', 'assets/images/1000w-8bit/flowers/taraxacum officinale.png', 0.5*this.componentWidth, 0.5*this.componentHeight, 14, false));
-    this.flowers_A.push(new Flower('c', 'assets/images/1000w-8bit/flowers/solidago rigida.png', 0.5*this.componentWidth, 0.5*this.componentHeight, 18, false));
-    this.flowers_A.push(new Flower('d', 'assets/images/1000w-8bit/flowers/sunflower.png', 0.5*this.componentWidth, 0.5*this.componentHeight, 14, false));
-    this.flowers_A.push(new Flower('e', 'assets/images/1000w-8bit/flowers/black raspberry.png', 0.5*this.componentWidth, 0.5*this.componentHeight, 12, false));
-    this.flowers_A.push(new Flower('f', 'assets/images/1000w-8bit/flowers/trifolium repens.png', 0.5*this.componentWidth, 0.5*this.componentHeight, 14, false));
-    this.flowers_A.push(new Flower('g', 'assets/images/1000w-8bit/flowers/vaccinium angustifolium.png', 0.5*this.componentWidth, 0.5*this.componentHeight, 22, false));
+    this.flowersA.push(new Flower(
+      'bee_a', 'assets/images/1000w-8bit/bees/rusty patch bumblebee.png', 0.5 * this.componentWidth, 0.5 * this.componentHeight, 6, false));
+    this.flowersA.push(new Flower(
+      'bee_b', 'assets/images/1000w-8bit/bees/megachile pugnata.png', 0.5 * this.componentWidth, 0.5 * this.componentHeight, 6, false));
+    this.flowersA.push(new Flower(
+      'bee_c', 'assets/images/1000w-8bit/bees/colletes simulans.png', 0.5 * this.componentWidth, 0.5 * this.componentHeight, 7, false));
+    this.flowersA.push(new Flower(
+      'a',  'assets/images/1000w-8bit/flowers/rudbeckia hirta.png', 0.5 * this.componentWidth, 0.5 * this.componentHeight, 25, false));
+    this.flowersA.push(new Flower(
+      'b', 'assets/images/1000w-8bit/flowers/taraxacum officinale.png', 0.5 * this.componentWidth, 0.5 * this.componentHeight, 14, false));
+    this.flowersA.push(new Flower(
+      'c', 'assets/images/1000w-8bit/flowers/solidago rigida.png', 0.5 * this.componentWidth, 0.5 * this.componentHeight, 18, false));
+    this.flowersA.push(new Flower(
+      'd', 'assets/images/1000w-8bit/flowers/sunflower.png', 0.5 * this.componentWidth, 0.5 * this.componentHeight, 14, false));
+    this.flowersA.push(new Flower(
+      'e', 'assets/images/1000w-8bit/flowers/black raspberry.png', 0.5 * this.componentWidth, 0.5 * this.componentHeight, 12, false));
+    this.flowersA.push(new Flower(
+      'f', 'assets/images/1000w-8bit/flowers/trifolium repens.png', 0.5 * this.componentWidth, 0.5 * this.componentHeight, 14, false));
+    this.flowersA.push(new Flower(
+      'g', 'assets/images/1000w-8bit/flowers/vaccinium angustifolium.png', 0.5 * this.componentWidth, 0.5 * this.componentHeight, 22,
+      false));
 
-    this.flowers_B.push(new Flower('bee_a', 'assets/images/1000w-8bit/bees/rusty patch bumblebee.png', 0.8*this.componentWidth, 0.735*this.componentHeight, 6, true));
-    this.flowers_B.push(new Flower('bee_b', 'assets/images/1000w-8bit/bees/megachile pugnata.png', 0.8*this.componentWidth, 0.63*this.componentHeight, 6, true));
-    this.flowers_B.push(new Flower('bee_c', 'assets/images/1000w-8bit/bees/colletes simulans.png', 0.8*this.componentWidth, 0.55*this.componentHeight, 7, true));
-    this.flowers_B.push(new Flower('a', 'assets/images/1000w-8bit/flowers/rudbeckia hirta.png', 0.6*this.componentWidth, 0.75*this.componentHeight, 25, true));
-    this.flowers_B.push(new Flower('b', 'assets/images/1000w-8bit/flowers/taraxacum officinale.png', 0.4*this.componentWidth, 0.7*this.componentHeight, 14, true));
-    this.flowers_B.push(new Flower('c', 'assets/images/1000w-8bit/flowers/solidago rigida.png', 0.2*this.componentWidth, 0.7*this.componentHeight, 18, true));
-    this.flowers_B.push(new Flower('d', 'assets/images/1000w-8bit/flowers/sunflower.png', 0.8*this.componentWidth, 0.25*this.componentHeight, 14, true));
-    this.flowers_B.push(new Flower('e', 'assets/images/1000w-8bit/flowers/black raspberry.png', 0.6*this.componentWidth, 0.25*this.componentHeight, 12, true));
-    this.flowers_B.push(new Flower('f', 'assets/images/1000w-8bit/flowers/trifolium repens.png', 0.4*this.componentWidth, 0.3*this.componentHeight, 14, true));
-    this.flowers_B.push(new Flower('g', 'assets/images/1000w-8bit/flowers/vaccinium angustifolium.png', 0.2*this.componentWidth, 0.3*this.componentHeight, 22, true));
+    this.flowersB.push(new Flower(
+      'bee_a', 'assets/images/1000w-8bit/bees/rusty patch bumblebee.png', 0.8 * this.componentWidth, 0.735 * this.componentHeight, 6,
+      true));
+    this.flowersB.push(new Flower(
+      'bee_b', 'assets/images/1000w-8bit/bees/megachile pugnata.png', 0.8 * this.componentWidth, 0.63 * this.componentHeight, 6, true));
+    this.flowersB.push(new Flower(
+      'bee_c', 'assets/images/1000w-8bit/bees/colletes simulans.png', 0.8 * this.componentWidth, 0.55 * this.componentHeight, 7, true));
+    this.flowersB.push(new Flower(
+      'a', 'assets/images/1000w-8bit/flowers/rudbeckia hirta.png', 0.6 * this.componentWidth, 0.75 * this.componentHeight, 25, true));
+    this.flowersB.push(new Flower(
+      'b', 'assets/images/1000w-8bit/flowers/taraxacum officinale.png', 0.4 * this.componentWidth, 0.7 * this.componentHeight, 14, true));
+    this.flowersB.push(new Flower(
+      'c', 'assets/images/1000w-8bit/flowers/solidago rigida.png', 0.2 * this.componentWidth, 0.7 * this.componentHeight, 18, true));
+    this.flowersB.push(new Flower(
+      'd', 'assets/images/1000w-8bit/flowers/sunflower.png', 0.8 * this.componentWidth, 0.25 * this.componentHeight, 14, true));
+    this.flowersB.push(new Flower(
+      'e', 'assets/images/1000w-8bit/flowers/black raspberry.png', 0.6 * this.componentWidth, 0.25 * this.componentHeight, 12, true));
+    this.flowersB.push(new Flower(
+      'f', 'assets/images/1000w-8bit/flowers/trifolium repens.png', 0.4 * this.componentWidth, 0.3 * this.componentHeight, 14, true));
+    this.flowersB.push(new Flower(
+      'g', 'assets/images/1000w-8bit/flowers/vaccinium angustifolium.png', 0.2 * this.componentWidth, 0.3 * this.componentHeight, 22,
+      true));
   }
 
   sleep(ms) {
@@ -97,8 +132,8 @@ export class LargeDisplayComponent implements OnInit {
   public testReactivate(id: string) {
 
     const current = this.flowers.filter(f => f.id === id)[0];
-    const targetA = this.flowers_A.filter(f => f.id === id)[0];
-    const targetB = this.flowers_B.filter(f => f.id === id)[0];
+    const targetA = this.flowersA.filter(f => f.id === id)[0];
+    const targetB = this.flowersB.filter(f => f.id === id)[0];
 
     if (current.displayState === 'normal') {
       current.displayState = 'normal_2';
