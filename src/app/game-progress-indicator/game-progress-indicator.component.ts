b82ced388019896db60ca8f6a588cb408e278809
Waller Li <lsw626313@gmail.com>
2020-03-19 22:19:30 -0500

    Add game progress spinner

--- src/app/game-progress-indicator/game-progress-indicator.component.ts
+++ src/app/game-progress-indicator/game-progress-indicator.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-game-progress-indicator',
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameProgressIndicatorComponent implements OnInit {
 
+  @Input() gameTime: number;
+
+  @Input() gameLength: number;
+
+  @Input() gameMonth: string;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  spinnerProgress(): number {
+    return 100 * (1 - this.gameTime / this.gameLength);
   }
 
 }
