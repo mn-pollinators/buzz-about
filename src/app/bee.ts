423f88be37867ffec5ea55843c720c04ff6e5526
Waller Li <lsw626313@gmail.com>
2020-04-02 05:57:07 -0500

    Adjust non-required fields in flower and bee interfaces

--- src/app/bee.ts
+++ src/app/bee.ts
@@ -1,11 +1,11 @@
 export interface Bee {
   id: string;
   species: string;
-  imgSrc: string;
-  active: boolean;
-  live: boolean;
-  currentFlower: string;
-  buzzingPeriods: {from: string, to: string}[];
-  health: number;
-  visible: boolean;
+  imgSrc?: string;
+  active?: boolean;
+  live?: boolean;
+  currentFlower?: string;
+  buzzingPeriods?: {from: string, to: string}[];
+  health?: number;
+  visible?: boolean;
 }
--- src/app/flower.ts
+++ src/app/flower.ts
@@ -1,9 +1,9 @@
 export interface Flower {
   id: string;
   species: string;
-  imgSrc: string;
-  blooming: boolean;
-  openPeriods: {from: string, to: string}[];
-  visible: boolean;
-  occupied: boolean;
+  imgSrc?: string;
+  blooming?: boolean;
+  openPeriods?: {from: string, to: string}[];
+  visible?: boolean;
+  occupied?: boolean;
 }
