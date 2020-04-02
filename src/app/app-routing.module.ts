5eb49c0af453044641512f1bbf05eb98f1eb7776
Waller Li <lsw626313@gmail.com>
2020-04-02 04:31:18 -0500

    Add review page to routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PlayRoundComponent } from './play-round/play-round.component';
 import { LargeDisplayComponent } from './large-display/large-display.component';
+import { GameReviewPageComponent } from './game-review-page/game-review-page.component';
 
 const routes: Routes = [
-  {path: "", component: PlayRoundComponent},
-  {path: "teacher", component: LargeDisplayComponent}
+  {path: '', component: PlayRoundComponent},
+  {path: 'teacher', component: LargeDisplayComponent},
+  {path: 'review', component: GameReviewPageComponent}
 ];
 
 @NgModule({
