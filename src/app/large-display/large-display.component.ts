4aeac7324c9b38b3a394b8867aaba76c2bd0600f
Waller Li <lsw626313@gmail.com>
2020-04-14 00:03:40 -0500

    Add updateDiscrepancy function to shift images automatically

--- src/app/large-display/large-display.component.ts
+++ src/app/large-display/large-display.component.ts
@@ -34,18 +34,23 @@ export class LargeDisplayComponent implements OnInit, AfterViewInit {
     this.updatePositions(this.flowers);
 
     this.componentWidth = window.innerWidth;
-    // this.componentHeight = this.componentWidth * 9 / 16;
+    this.componentHeight = window.innerHeight;
     // document.getElementById('largeDisplayContent').style.paddingTop =
       // Math.max(0, (window.innerHeight - this.componentHeight) / 2) + 'px';
   }
 
   ngOnInit() {
     this.componentWidth = window.innerWidth;
+    this.componentHeight = window.innerHeight;
     this.initializeDemoFlowers();
 
     // this.flowers = this.demo1;
     this.flowers = this.demo2;
     // this.flowers = this.demo3;
+
+    for (const f of this.flowers) {
+      this.updateDiscrepancy(f);
+    }
   }
 
   ngAfterViewInit() {
@@ -54,6 +59,32 @@ export class LargeDisplayComponent implements OnInit, AfterViewInit {
       this.gameMonth = this.timerBar.getMonth();
       this.gameTime = this.timerBar.getTime();
     }, 1000);
+  }
+
+  updateDiscrepancy(s: DisplaySpecies) {
+    let currentD = 0;
+    setInterval(() => {
+      const r = this.componentWidth / this.componentHeight;
+      if (r > 1.8 && currentD !== 0) {
+        s.moveTo(s.initialX, s.initialY - currentD, this.componentWidth);
+        currentD = 0;
+      } else if (r <= 1.8 && r > 1.68 && currentD !== 0.05) {
+        s.moveTo(s.initialX, s.initialY - currentD + 0.05, this.componentWidth);
+        currentD = 0.05;
+      } else if (r <= 1.68 && r > 1.56 && currentD !== 0.1) {
+        s.moveTo(s.initialX, s.initialY - currentD + 0.1, this.componentWidth);
+        currentD = 0.1;
+      } else if (r <= 1.56 && r > 1.44 && currentD !== 0.15) {
+        s.moveTo(s.initialX, s.initialY - currentD + 0.15, this.componentWidth);
+        currentD = 0.15;
+      } else if (r <= 1.44 && r > 1.32 && currentD !== 0.2) {
+        s.moveTo(s.initialX, s.initialY - currentD + 0.2, this.componentWidth);
+        currentD = 0.2;
+      } else if (r <= 1.32 && currentD !== 0.2) {
+        s.moveTo(s.initialX, s.initialY - currentD + 0.25, this.componentWidth);
+        currentD = 0.25;
+      }
+    }, 500);
   }
 
   updatePositions(flowers: DisplaySpecies[]) {
@@ -76,48 +107,48 @@ export class LargeDisplayComponent implements OnInit, AfterViewInit {
 
 
     this.demo2.push(new DisplaySpecies('a',  'assets/images/1000w-8bit/flowers/rudbeckia hirta.png',
-      0.08, 0.45, 19, true, this.componentWidth));
+      0.08, 0.4, 19, true, this.componentWidth));
     this.demo2.push(new DisplaySpecies('b', 'assets/images/1000w-8bit/flowers/taraxacum officinale.png',
-      0.2, 0.39, 11, true, this.componentWidth));
+      0.2, 0.34, 11, true, this.componentWidth));
     this.demo2.push(new DisplaySpecies('c', 'assets/images/1000w-8bit/flowers/solidago rigida.png',
-      0.32, 0.47, 13, true, this.componentWidth));
+      0.32, 0.37, 13, true, this.componentWidth));
     this.demo2.push(new DisplaySpecies('d', 'assets/images/1000w-8bit/flowers/sunflower.png',
-      0.44, 0.37, 10, true, this.componentWidth));
+      0.44, 0.32, 10, true, this.componentWidth));
     this.demo2.push(new DisplaySpecies('e', 'assets/images/1000w-8bit/flowers/black raspberry.png',
-      0.54, 0.5, 10, true, this.componentWidth));
+      0.54, 0.4, 10, true, this.componentWidth));
     this.demo2.push(new DisplaySpecies('f', 'assets/images/1000w-8bit/flowers/trifolium repens.png',
-      0.65, 0.4, 11, true, this.componentWidth));
+      0.65, 0.34, 11, true, this.componentWidth));
     this.demo2.push(new DisplaySpecies('g', 'assets/images/1000w-8bit/flowers/vaccinium angustifolium.png',
-      0.77, 0.49, 15, true, this.componentWidth));
+      0.77, 0.4, 15, true, this.componentWidth));
 
     this.demo2.push(new DisplaySpecies('h',  'assets/images/1000w-8bit/flowers/rudbeckia hirta.png',
-      0.895, 0.48, 21, true, this.componentWidth));
+      0.87, 0.46, 21, true, this.componentWidth));
     this.demo2.push(new DisplaySpecies('i', 'assets/images/1000w-8bit/flowers/taraxacum officinale.png',
-      0.89, 0.7, 11, true, this.componentWidth));
+      0.87, 0.68, 11, true, this.componentWidth));
     this.demo2.push(new DisplaySpecies('j', 'assets/images/1000w-8bit/flowers/solidago rigida.png',
-      0.73, 0.78, 13, true, this.componentWidth));
+      0.73, 0.63, 13, true, this.componentWidth));
     this.demo2.push(new DisplaySpecies('k', 'assets/images/1000w-8bit/flowers/sunflower.png',
-      0.62, 0.72, 10, true, this.componentWidth));
+      0.62, 0.62, 10, true, this.componentWidth));
     this.demo2.push(new DisplaySpecies('l', 'assets/images/1000w-8bit/flowers/black raspberry.png',
-      0.06, 0.65, 9, true, this.componentWidth));
+      0.08, 0.58, 9, true, this.componentWidth));
     this.demo2.push(new DisplaySpecies('m', 'assets/images/1000w-8bit/flowers/trifolium repens.png',
-      0.48, 0.85, 11, true, this.componentWidth));
+      0.48, 0.7, 11, true, this.componentWidth));
     this.demo2.push(new DisplaySpecies('n', 'assets/images/1000w-8bit/flowers/vaccinium angustifolium.png',
-      0.4, 0.69, 15, true, this.componentWidth));
+      0.4, 0.59, 15, true, this.componentWidth));
 
     this.demo2.push(new DisplaySpecies('k', 'assets/images/1000w-8bit/flowers/sunflower.png',
-      0.17, 0.8, 10, true, this.componentWidth));
+      0.19, 0.665, 10, true, this.componentWidth));
     this.demo2.push(new DisplaySpecies('l', 'assets/images/1000w-8bit/flowers/black raspberry.png',
-      0.3, 0.72, 9, true, this.componentWidth));
+      0.3, 0.62, 9, true, this.componentWidth));
 
 
 
     this.demo3.push(new DisplaySpecies('bee_a', 'assets/images/1000w-8bit/bees/rusty patch bumblebee.png',
        0.5, 0.5 , 6, false, this.componentWidth));
-    this.demo3.push(new DisplaySpecies('bee_b', 'assets/images/1000w-8bit/bees/megachile pugnata.png', 0.5,
-       0.5 , 6, false, this.componentWidth));
-    this.demo3.push(new DisplaySpecies('bee_c', 'assets/images/1000w-8bit/bees/colletes simulans.png', 0.5,
-       0.5 , 7, false, this.componentWidth));
+    this.demo3.push(new DisplaySpecies('bee_b', 'assets/images/1000w-8bit/bees/megachile pugnata.png',
+      0.5, 0.5 , 6, false, this.componentWidth));
+    this.demo3.push(new DisplaySpecies('bee_c', 'assets/images/1000w-8bit/bees/colletes simulans.png',
+      0.5, 0.5 , 7, false, this.componentWidth));
   }
 
   sleep(ms) {
