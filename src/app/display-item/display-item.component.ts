7824484d7e5a4b01305b2e6f3d5f8631085299e4
Waller Li <lsw626313@gmail.com>
2020-04-17 00:28:02 -0500

    Fix error in position auto-adjustment

--- src/app/display-item/display-item.component.ts
+++ src/app/display-item/display-item.component.ts
@@ -47,15 +47,28 @@ export class DisplayItemComponent implements AfterViewInit {
 
   componentWidth = window.innerWidth;
   componentHeight = window.innerHeight;
+  discrepancyChangeCounter = 0;
+  sizeChangeCounter = 0;
+  currentDiscrepancy = 0;
+  screenRatio = 1;
 
   @HostListener('window:resize', ['$event']) onResize() {
     this.updatePositions();
     this.componentWidth = window.innerWidth;
     this.componentHeight = window.innerHeight;
+
+    this.discrepancyChangeCounter++;
+    setTimeout(() => {
+      this.discrepancyChangeCounter--;
+      console.log(this.discrepancyChangeCounter);
+      if (this.discrepancyChangeCounter === 0) {
+        this.updateDiscrepancy();
+      }
+    }, 500);
   }
 
   ngAfterViewInit(): void {
-    setTimeout(() => this.updateDiscrepancy(), 100);
+    setTimeout(() => this.updateDiscrepancy(), 800);
   }
 
   updatePositions() {
@@ -64,28 +77,25 @@ export class DisplayItemComponent implements AfterViewInit {
   }
 
   updateDiscrepancy() {
-    let currentD = 0;
-    setInterval(() => {
-      const r = this.componentWidth / this.componentHeight;
-      if (r > 1.8 && currentD !== 0) {
-        this.species.moveTo(this.species.initialX, this.species.initialY - currentD, this.componentWidth);
-        currentD = 0;
-      } else if (r <= 1.8 && r > 1.68 && currentD !== 0.05) {
-        this.species.moveTo(this.species.initialX, this.species.initialY - currentD + 0.05, this.componentWidth);
-        currentD = 0.05;
-      } else if (r <= 1.68 && r > 1.56 && currentD !== 0.1) {
-        this.species.moveTo(this.species.initialX, this.species.initialY - currentD + 0.1, this.componentWidth);
-        currentD = 0.1;
-      } else if (r <= 1.56 && r > 1.44 && currentD !== 0.15) {
-        this.species.moveTo(this.species.initialX, this.species.initialY - currentD + 0.15, this.componentWidth);
-        currentD = 0.15;
-      } else if (r <= 1.44 && r > 1.32 && currentD !== 0.2) {
-        this.species.moveTo(this.species.initialX, this.species.initialY - currentD + 0.2, this.componentWidth);
-        currentD = 0.2;
-      } else if (r <= 1.32 && currentD !== 0.2) {
-        this.species.moveTo(this.species.initialX, this.species.initialY - currentD + 0.25, this.componentWidth);
-        currentD = 0.25;
-      }
-    }, 500);
+    this.screenRatio = this.componentWidth / this.componentHeight;
+    if (this.screenRatio > 1.8 && this.currentDiscrepancy !== 0) {
+      this.species.moveTo(this.species.initialX, this.species.initialY - this.currentDiscrepancy, this.componentWidth);
+      this.currentDiscrepancy = 0;
+    } else if (this.screenRatio <= 1.8 && this.screenRatio > 1.68 && this.currentDiscrepancy !== 0.05) {
+      this.species.moveTo(this.species.initialX, this.species.initialY - this.currentDiscrepancy + 0.05, this.componentWidth);
+      this.currentDiscrepancy = 0.05;
+    } else if (this.screenRatio <= 1.68 && this.screenRatio > 1.56 && this.currentDiscrepancy !== 0.1) {
+      this.species.moveTo(this.species.initialX, this.species.initialY - this.currentDiscrepancy + 0.1, this.componentWidth);
+      this.currentDiscrepancy = 0.1;
+    } else if (this.screenRatio <= 1.56 && this.screenRatio > 1.44 && this.currentDiscrepancy !== 0.15) {
+      this.species.moveTo(this.species.initialX, this.species.initialY - this.currentDiscrepancy + 0.15, this.componentWidth);
+      this.currentDiscrepancy = 0.15;
+    } else if (this.screenRatio <= 1.44 && this.screenRatio > 1.32 && this.currentDiscrepancy !== 0.2) {
+      this.species.moveTo(this.species.initialX, this.species.initialY - this.currentDiscrepancy + 0.2, this.componentWidth);
+      this.currentDiscrepancy = 0.2;
+    } else if (this.screenRatio <= 1.32 && this.currentDiscrepancy !== 0.2) {
+      this.species.moveTo(this.species.initialX, this.species.initialY - this.currentDiscrepancy + 0.25, this.componentWidth);
+      this.currentDiscrepancy = 0.25;
+    }
   }
 }
