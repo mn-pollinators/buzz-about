6ea22c5c77d8355e5231dea2ffc03e7ababdf8a8
Waller Li <lsw626313@gmail.com>
2020-05-17 20:34:25 -0500

    Simplify update activeness and remove unused demos

--- src/app/large-display/large-display.component.ts
+++ src/app/large-display/large-display.component.ts
@@ -16,9 +16,7 @@ export class LargeDisplayComponent implements OnInit, AfterViewInit {
   @ViewChild(TimerBarComponent, {static: false}) private timerBar: TimerBarComponent;
 
   currentDisplayed: DisplaySpecies[] = new Array<DisplaySpecies>();
-  demo1: DisplaySpecies[] = new Array<DisplaySpecies>();
-  demo2: DisplaySpecies[] = new Array<DisplaySpecies>();
-  demo3: DisplaySpecies[] = new Array<DisplaySpecies>();
+  demoFlowers: DisplaySpecies[] = new Array<DisplaySpecies>();
 
   gameLength = 120;
   gameTime = 0;
@@ -42,7 +40,7 @@ export class LargeDisplayComponent implements OnInit, AfterViewInit {
 
   ngOnInit() {
     this.initializeDemoFlowers();
-    this.currentDisplayed = this.demo2;
+    this.currentDisplayed = this.demoFlowers;
   }
 
   ngAfterViewInit() {
@@ -51,19 +49,13 @@ export class LargeDisplayComponent implements OnInit, AfterViewInit {
       setInterval(() => {
         const newMonth = this.timerBar.getMonth();
         if (this.gameMonth.main !== newMonth.main || this.gameMonth.sub !== newMonth.sub) {
-          this.updateActiveness(newMonth);
+          this.currentDisplayed.forEach(s => s.updateActiveness(newMonth));
         }
-        this.gameRunning = this.timerBar.getStatus();
-        this.gameMonth = this.timerBar.getMonth();
+        this.gameMonth = newMonth;
         this.gameTime = this.timerBar.getTime();
+        this.gameRunning = this.timerBar.getStatus();
       }, 1000);
     }, 1000);
-  }
-
-  updateActiveness(month: GameMonth) {
-    this.currentDisplayed.forEach(s => {
-      s.updateActiveness(month);
-    });
   }
 
   suggestResize() {
@@ -80,35 +72,35 @@ export class LargeDisplayComponent implements OnInit, AfterViewInit {
   }
 
   initializeDemoFlowers() {
-    this.demo2.push(new DisplaySpecies(
+    this.demoFlowers.push(new DisplaySpecies(
       {id: 'a', species: 'rudbeckia hirta',
         imgSrc: 'assets/images/1000w-8bit/flowers/rudbeckia hirta.png',
         activePeriods: [
           {from: {sub: 'early-', main: 'April'} as GameMonth, to: {sub: 'mid-', main: 'June'} as GameMonth}
         ]} as Flower,
       0.08, 0.4, 19, this.componentWidth));
-    this.demo2.push(new DisplaySpecies(
+    this.demoFlowers.push(new DisplaySpecies(
       {id: 'b', species: 'taraxacum officinale',
         imgSrc: 'assets/images/1000w-8bit/flowers/taraxacum officinale.png',
         activePeriods: [
           {from: {sub: '', main: 'March'} as GameMonth, to: {sub: '', main: 'April'} as GameMonth}
         ]} as Flower,
       0.2, 0.34, 11, this.componentWidth));
-    this.demo2.push(new DisplaySpecies(
+    this.demoFlowers.push(new DisplaySpecies(
       {id: 'c', species: 'solidago rigida',
         imgSrc: 'assets/images/1000w-8bit/flowers/solidago rigida.png',
         activePeriods: [
           {from: {sub: 'mid-', main: 'July'} as GameMonth, to: {sub: 'late-', main: 'October'} as GameMonth}
         ]} as Flower,
       0.32, 0.37, 13, this.componentWidth));
-    this.demo2.push(new DisplaySpecies(
+    this.demoFlowers.push(new DisplaySpecies(
       {id: 'd', species: 'sunflower',
         imgSrc: 'assets/images/1000w-8bit/flowers/sunflower.png',
         activePeriods: [
           {from: {sub: 'mid-', main: 'April'} as GameMonth, to: {sub: '', main: 'May'} as GameMonth}
         ]} as Flower,
       0.45, 0.3, 10, this.componentWidth));
-    this.demo2.push(new DisplaySpecies(
+    this.demoFlowers.push(new DisplaySpecies(
       {id: 'e', species: 'black raspberry',
         imgSrc: 'assets/images/1000w-8bit/flowers/black raspberry.png',
         activePeriods: [
@@ -116,14 +108,14 @@ export class LargeDisplayComponent implements OnInit, AfterViewInit {
           {from: {sub: 'late-', main: 'August'} as GameMonth, to: {sub: '', main: 'November'} as GameMonth}
         ]} as Flower,
       0.54, 0.4, 10, this.componentWidth));
-    this.demo2.push(new DisplaySpecies(
+    this.demoFlowers.push(new DisplaySpecies(
       {id: 'f', species: 'trifolium repens',
         imgSrc: 'assets/images/1000w-8bit/flowers/trifolium repens.png',
         activePeriods: [
           {from: {sub: 'late-', main: 'May'} as GameMonth, to: {sub: 'mid-', main: 'September'} as GameMonth}
         ]} as Flower,
       0.65, 0.34, 11, this.componentWidth));
-    this.demo2.push(new DisplaySpecies(
+    this.demoFlowers.push(new DisplaySpecies(
       {id: 'g', species: 'vaccinium angustifolium',
         imgSrc: 'assets/images/1000w-8bit/flowers/vaccinium angustifolium.png',
         activePeriods: [
@@ -131,35 +123,35 @@ export class LargeDisplayComponent implements OnInit, AfterViewInit {
         ]} as Flower,
       0.77, 0.4, 15, this.componentWidth));
 
-    this.demo2.push(new DisplaySpecies(
+    this.demoFlowers.push(new DisplaySpecies(
       {id: 'h', species: 'rudbeckia hirta',
         imgSrc: 'assets/images/1000w-8bit/flowers/rudbeckia hirta.png',
         activePeriods: [
           {from: {sub: 'early-', main: 'April'} as GameMonth, to: {sub: 'mid-', main: 'June'} as GameMonth}
         ]} as Flower,
       0.73, 0.63, 21, this.componentWidth));
-    this.demo2.push(new DisplaySpecies(
+    this.demoFlowers.push(new DisplaySpecies(
       {id: 'i', species: 'taraxacum officinale',
         imgSrc: 'assets/images/1000w-8bit/flowers/taraxacum officinale.png',
         activePeriods: [
           {from: {sub: '', main: 'March'} as GameMonth, to: {sub: '', main: 'April'} as GameMonth}
         ]} as Flower,
       0.88, 0.68, 11, this.componentWidth));
-    this.demo2.push(new DisplaySpecies(
+    this.demoFlowers.push(new DisplaySpecies(
       {id: 'j', species: 'solidago rigida',
         imgSrc: 'assets/images/1000w-8bit/flowers/solidago rigida.png',
         activePeriods: [
           {from: {sub: 'mid-', main: 'July'} as GameMonth, to: {sub: 'late-', main: 'October'} as GameMonth}
         ]} as Flower,
       0.90, 0.45, 13, this.componentWidth));
-    this.demo2.push(new DisplaySpecies(
+    this.demoFlowers.push(new DisplaySpecies(
       {id: 'k', species: 'sunflower',
         imgSrc: 'assets/images/1000w-8bit/flowers/sunflower.png',
         activePeriods: [
           {from: {sub: 'mid-', main: 'April'} as GameMonth, to: {sub: '', main: 'May'} as GameMonth}
         ]} as Flower,
       0.62, 0.62, 10, this.componentWidth));
-    this.demo2.push(new DisplaySpecies(
+    this.demoFlowers.push(new DisplaySpecies(
       {id: 'l', species: 'black raspberry',
         imgSrc: 'assets/images/1000w-8bit/flowers/black raspberry.png',
         activePeriods: [
@@ -167,14 +159,14 @@ export class LargeDisplayComponent implements OnInit, AfterViewInit {
           {from: {sub: 'late-', main: 'August'} as GameMonth, to: {sub: '', main: 'November'} as GameMonth}
         ]} as Flower,
       0.08, 0.58, 9, this.componentWidth));
-    this.demo2.push(new DisplaySpecies(
+    this.demoFlowers.push(new DisplaySpecies(
       {id: 'm', species: 'trifolium repens',
         imgSrc: 'assets/images/1000w-8bit/flowers/trifolium repens.png',
         activePeriods: [
           {from: {sub: 'late-', main: 'May'} as GameMonth, to: {sub: 'mid-', main: 'September'} as GameMonth}
         ]} as Flower,
       0.5, 0.7, 11, this.componentWidth));
-    this.demo2.push(new DisplaySpecies(
+    this.demoFlowers.push(new DisplaySpecies(
       {id: 'n', species: 'vaccinium angustifolium',
         imgSrc: 'assets/images/1000w-8bit/flowers/vaccinium angustifolium.png',
         activePeriods: [
@@ -182,14 +174,14 @@ export class LargeDisplayComponent implements OnInit, AfterViewInit {
         ]} as Flower,
       0.4, 0.59, 15, this.componentWidth));
 
-    this.demo2.push(new DisplaySpecies(
+    this.demoFlowers.push(new DisplaySpecies(
       {id: 'o', species: 'sunflower',
         imgSrc: 'assets/images/1000w-8bit/flowers/sunflower.png',
         activePeriods: [
           {from: {sub: 'mid-', main: 'April'} as GameMonth, to: {sub: '', main: 'May'} as GameMonth}
         ]} as Flower,
       0.19, 0.665, 10, this.componentWidth));
-    this.demo2.push(new DisplaySpecies(
+    this.demoFlowers.push(new DisplaySpecies(
       {id: 'p', species: 'black raspberry',
         imgSrc: 'assets/images/1000w-8bit/flowers/black raspberry.png',
         activePeriods: [
