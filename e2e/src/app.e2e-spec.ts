0c2965b5d2c2439bd9bb3a13a9a823feeff2015c
Paul Friederichsen <floogulinc@gmail.com>
2020-05-21 14:24:26 -0500

    Rename to Buzz About
    Co-authored-by: Joe Walbran <walbr037@morris.umn.edu>

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to ar-simulation!');
+    expect(page.getTitleText()).toEqual('Welcome to buzz-about!');
   });
 
   afterEach(async () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ar-simulation';
+  title = 'Buzz About';
 
   collapseBar: boolean = false;
 
