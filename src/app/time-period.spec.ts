3a86937b51308490e6b42b97996e64b682834e2a
Joe Walbran <walbr037@morris.umn.edu>
2020-05-25 14:50:51 -0500

    Change TimePeriod to use a Month enum and one-based indexing
    As discussed in PR #29.
    
    These tests are currently failing, because we haven't actually updated TimePeriod to conform to the new interface. (We're going to do that next.)

--- src/app/time-period.spec.ts
+++ src/app/time-period.spec.ts
@@ -1,30 +1,30 @@
-import { TimePeriod } from './time-period';
+import { TimePeriod, Month } from './time-period';
 
 describe('TimePeriod', () => {
   describe('The fromIsoDate() method', () => {
     describe('When you pass in a valid input', () => {
       it('Correctly parses --01-01', () => {
         const timePeriod = TimePeriod.fromIsoDate('--01-01');
-        expect(timePeriod.month).toEqual(0);
-        expect(timePeriod.quarter).toEqual(0);
+        expect(timePeriod.month).toEqual(Month.January);
+        expect(timePeriod.quarter).toEqual(1);
       });
 
       it('Correctly parses --01-31', () => {
         const timePeriod = TimePeriod.fromIsoDate('--01-31');
-        expect(timePeriod.month).toEqual(0);
-        expect(timePeriod.quarter).toEqual(3);
+        expect(timePeriod.month).toEqual(Month.January);
+        expect(timePeriod.quarter).toEqual(4);
       });
 
       it('Correctly parses --06-10', () => {
         const timePeriod = TimePeriod.fromIsoDate('--06-10');
-        expect(timePeriod.month).toEqual(5);
-        expect(timePeriod.quarter).toEqual(1);
+        expect(timePeriod.month).toEqual(Month.June);
+        expect(timePeriod.quarter).toEqual(2);
       });
 
       it('Correctly parses --12-31', () => {
         const timePeriod = TimePeriod.fromIsoDate('--12-31');
-        expect(timePeriod.month).toEqual(11);
-        expect(timePeriod.quarter).toEqual(3);
+        expect(timePeriod.month).toEqual(Month.December);
+        expect(timePeriod.quarter).toEqual(4);
       });
     });
 
@@ -59,44 +59,44 @@ describe('TimePeriod', () => {
 
   describe('The fromMonthAndQuarter() method', () => {
     it('Correctly creates the initial time period', () => {
-      const timePeriod = TimePeriod.fromMonthAndQuarter(0, 0);
+      const timePeriod = TimePeriod.fromMonthAndQuarter(Month.January, 1);
       expect(timePeriod.time).toEqual(0);
-      expect(timePeriod.month).toEqual(0);
-      expect(timePeriod.quarter).toEqual(0);
+      expect(timePeriod.month).toEqual(Month.January);
+      expect(timePeriod.quarter).toEqual(1);
       expect(timePeriod.monthString).toEqual('January');
     });
 
-    it('Correctly creates a time period (2,1)', () => {
-      const timePeriod = TimePeriod.fromMonthAndQuarter(2, 1);
+    it('Correctly creates the second quarter of March', () => {
+      const timePeriod = TimePeriod.fromMonthAndQuarter(Month.March, 2);
       expect(timePeriod.time).toEqual(9);
-      expect(timePeriod.month).toEqual(2);
-      expect(timePeriod.quarter).toEqual(1);
+      expect(timePeriod.month).toEqual(Month.March);
+      expect(timePeriod.quarter).toEqual(2);
       expect(timePeriod.monthString).toEqual('March');
     });
   });
 
   describe('The next() method', () => {
     it('Correctly increments the initial time period', () => {
-      const nextTimePeriod = TimePeriod.fromMonthAndQuarter(0, 0).next();
+      const nextTimePeriod = TimePeriod.fromMonthAndQuarter(Month.January, 1).next();
       expect(nextTimePeriod.time).toEqual(1);
-      expect(nextTimePeriod.month).toEqual(0);
-      expect(nextTimePeriod.quarter).toEqual(1);
+      expect(nextTimePeriod.month).toEqual(Month.January);
+      expect(nextTimePeriod.quarter).toEqual(2);
       expect(nextTimePeriod.monthString).toEqual('January');
     });
 
     it('Correctly increments the last quarter of January', () => {
-      const nextTimePeriod = TimePeriod.fromMonthAndQuarter(0, 3).next();
+      const nextTimePeriod = TimePeriod.fromMonthAndQuarter(Month.January, 4).next();
       expect(nextTimePeriod.time).toEqual(4);
-      expect(nextTimePeriod.month).toEqual(1);
-      expect(nextTimePeriod.quarter).toEqual(0);
+      expect(nextTimePeriod.month).toEqual(Month.February);
+      expect(nextTimePeriod.quarter).toEqual(1);
       expect(nextTimePeriod.monthString).toEqual('February');
     });
 
     it('Correctly wraps the last quarter of December', () => {
-      const nextTimePeriod = TimePeriod.fromMonthAndQuarter(11, 3).next();
+      const nextTimePeriod = TimePeriod.fromMonthAndQuarter(Month.December, 4).next();
       expect(nextTimePeriod.time).toEqual(0);
-      expect(nextTimePeriod.month).toEqual(0);
-      expect(nextTimePeriod.quarter).toEqual(0);
+      expect(nextTimePeriod.month).toEqual(Month.January);
+      expect(nextTimePeriod.quarter).toEqual(1);
       expect(nextTimePeriod.monthString).toEqual('January');
     });
   });
