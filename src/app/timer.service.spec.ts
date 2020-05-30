69b9832f42c8025a2c4886fe85158f236e8ff9cf
Paul Friederichsen <floogulinc@gmail.com>
2020-05-30 17:18:47 -0500

    Add testing for TimerService and fix a bug in it
    Co-authored-by: Joe Walbran <walbr037@morris.umn.edu>

--- src/app/timer.service.spec.ts
+++ src/app/timer.service.spec.ts
@@ -1,12 +1,200 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, async, fakeAsync, tick } from '@angular/core/testing';
 
-import { TimerService } from './timer.service';
+import { TimerService, TimerState } from './timer.service';
+import { TimePeriod } from './time-period';
+import { TestScheduler } from 'rxjs/testing';
+import { NEVER, of } from 'rxjs';
+
+describe('Sanity Tests', () => {
+  const testScheduler = new TestScheduler((actual, expected) => {
+    // asserting the two objects are equal
+    expect(actual).toEqual(expected);
+  });
+
+  it('tests of() correctly', async(() => {
+    testScheduler.run(({expectObservable}) => {
+      expectObservable(of('a')).toBe('(a|)');
+    });
+  }));
+
+  it('tests NEVER correctly', async(() => {
+    testScheduler.run(({expectObservable}) => {
+      expectObservable(NEVER).toBe('-');
+    });
+  }));
+});
+
 
 describe('TimerService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: TimerService;
 
-  it('should be created', () => {
-    const service: TimerService = TestBed.get(TimerService);
+  beforeEach(() => {
+    TestBed.configureTestingModule({providers: [TimerService]});
+    service = TestBed.get(TimerService); // TODO change to inject when we update to Angular 9
+  });
+
+  it('Should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  describe('the timerState$ observable', () => {
+    it('Does not emit the initial state when subscribed to', async(() => {
+      const testScheduler = new TestScheduler((actual, expected) => {
+        expect(actual).toEqual(expected);
+      });
+      testScheduler.run(({expectObservable}) => {
+        expectObservable(service.timerState$).toBe('-');
+      });
+    }));
+
+
+    it('Emits initial values', fakeAsync(() => {
+      const initialState = {
+        running: false,
+        tickSpeed: 2,
+        currentTime: TimePeriod.fromMonthAndQuarter(1, 1),
+        endTime: TimePeriod.fromMonthAndQuarter(1, 2)
+      };
+
+      let lastEmittedTimerState: TimerState;
+
+      service.timerState$.subscribe(state => {
+        lastEmittedTimerState = state;
+      });
+
+      service.initialize(initialState);
+      tick(0);
+      expect(lastEmittedTimerState).toEqual(initialState);
+    }));
+
+    it('Runs for 2 ticks', fakeAsync(() => {
+      const tickSpeed = 2;
+
+      const initialState = {
+        running: true,
+        tickSpeed,
+        currentTime: TimePeriod.fromMonthAndQuarter(1, 1),
+        endTime: TimePeriod.fromMonthAndQuarter(1, 2)
+      };
+
+      const subsequentStates = [
+        {
+          running: true,
+          tickSpeed,
+          currentTime: TimePeriod.fromMonthAndQuarter(1, 2),
+          endTime: TimePeriod.fromMonthAndQuarter(1, 2)
+        },
+        {
+          running: false,
+          tickSpeed,
+          currentTime: TimePeriod.fromMonthAndQuarter(1, 2),
+          endTime: TimePeriod.fromMonthAndQuarter(1, 2)
+        },
+      ];
+
+      let lastEmittedTimerState: TimerState;
+
+      service.timerState$.subscribe(state => {
+        lastEmittedTimerState = state;
+      });
+
+      service.initialize(initialState);
+      tick(0);
+      expect(lastEmittedTimerState).toEqual(initialState);
+
+      for (const state of subsequentStates) {
+        tick(tickSpeed);
+        expect(lastEmittedTimerState).toEqual(state);
+      }
+    }));
+
+
+    it('Emits twice if the start and the end are the same', fakeAsync(() => {
+      const tickSpeed = 2;
+
+      const initialState = {
+        running: true,
+        tickSpeed,
+        currentTime: TimePeriod.fromMonthAndQuarter(1, 1),
+        endTime: TimePeriod.fromMonthAndQuarter(1, 1)
+      };
+
+      const subsequentStates = [
+        {
+          running: false,
+          tickSpeed,
+          currentTime: TimePeriod.fromMonthAndQuarter(1, 1),
+          endTime: TimePeriod.fromMonthAndQuarter(1, 1)
+        },
+      ];
+
+      let lastEmittedTimerState: TimerState;
+
+      service.timerState$.subscribe(state => {
+        lastEmittedTimerState = state;
+      });
+
+      service.initialize(initialState);
+      tick(0);
+      expect(lastEmittedTimerState).toEqual(initialState);
+
+      for (const state of subsequentStates) {
+        tick(tickSpeed);
+        expect(lastEmittedTimerState).toEqual(state);
+      }
+    }));
+
+    it('Works fine if you initialize it as paused and then set running to true', fakeAsync(() => {
+      const tickSpeed = 2;
+
+      const initialState = {
+        running: false,
+        tickSpeed,
+        currentTime: TimePeriod.fromMonthAndQuarter(1, 1),
+        endTime: TimePeriod.fromMonthAndQuarter(1, 2)
+      };
+
+      const stateAfterYouSetRunningToTrue = {
+        running: true,
+        tickSpeed,
+        currentTime: TimePeriod.fromMonthAndQuarter(1, 1),
+        endTime: TimePeriod.fromMonthAndQuarter(1, 2)
+      };
+
+      const subsequentStates = [
+        {
+          running: true,
+          tickSpeed,
+          currentTime: TimePeriod.fromMonthAndQuarter(1, 2),
+          endTime: TimePeriod.fromMonthAndQuarter(1, 2)
+        },
+        {
+          running: false,
+          tickSpeed,
+          currentTime: TimePeriod.fromMonthAndQuarter(1, 2),
+          endTime: TimePeriod.fromMonthAndQuarter(1, 2)
+        },
+      ];
+
+      let lastEmittedTimerState: TimerState;
+
+      service.timerState$.subscribe(state => {
+        lastEmittedTimerState = state;
+      });
+
+      service.initialize(initialState);
+      tick(0);
+      expect(lastEmittedTimerState).toEqual(initialState);
+
+      service.setRunning(true);
+      tick(0);
+      expect(lastEmittedTimerState).toEqual(stateAfterYouSetRunningToTrue);
+
+      for (const state of subsequentStates) {
+        tick(tickSpeed);
+        expect(lastEmittedTimerState).toEqual(state);
+      }
+    }));
+  });
 });
+
--- src/app/timer.service.ts
+++ src/app/timer.service.ts
@@ -1,14 +1,33 @@
 import { Injectable } from '@angular/core';
-import { Subject, Observable, interval, NEVER, of, timer } from 'rxjs';
+import { Subject, Observable, interval, NEVER, of, timer, concat } from 'rxjs';
 import { startWith, scan, tap, share, switchMap, mapTo, map, distinctUntilChanged } from 'rxjs/operators';
 import { TimePeriod } from './time-period';
 
 
-interface TimerState {
+export interface TimerState {
+  /**
+   * Whether the timer is currently paused.
+   */
   running: boolean;
-  // ms per tick
+
+  /**
+   * How many milliseconds there are per tick.
+   */
   tickSpeed: number;
+
+  /**
+   * What the current TimePeriod is.
+   */
   currentTime: TimePeriod;
+
+  /**
+   * When the timer reaches this time period, it will emit a tick for the
+   * end time, and then pause itself.
+   *
+   * (The end time is an inclusive endpoint.)
+   *
+   * If `endTime` is null, the timer will run forever.
+   */
   endTime: TimePeriod;
 }
 
@@ -21,15 +40,30 @@ export class TimerService {
 
   timerState$: Observable<TimerState>;
 
+  /**
+   * What the TimePeriod is right now.
+   *
+   * This Observable emits whenever the TimePeriod changes.
+   */
   currentTime$: Observable<TimePeriod>;
 
+  /**
+   * Whether the timer is currently running.
+   *
+   * This Observable emits whenever the timer starts or stops.
+   */
   running$: Observable<boolean>;
 
 
-  // Start with a dummy value. (It doesn't really matter as long
-  // as it's paused--nobody is going to read from the timer until
-  // you call initialize().)
-  readonly DUMMY_INITIAL_TIMER_STATE: TimerState = {
+  /**
+   * This is the initial state of the timer. It's never actually emitted,
+   * but it's the point from which the timer starts counting.
+   *
+   * It's important that it begins paused, but other than that, the exact
+   * values don't really matter--whoever uses TimerService will call
+   * initialize() to configure whatever settings they need.
+   */
+  readonly INITIAL_TIMER_STATE: TimerState = {
     running: false,
     tickSpeed: 1,
     currentTime: TimePeriod.fromMonthAndQuarter(1, 1),
@@ -38,21 +72,23 @@ export class TimerService {
 
   constructor() {
     this.timerState$ = this.events$.pipe(
-      startWith(this.DUMMY_INITIAL_TIMER_STATE),
+      startWith(this.INITIAL_TIMER_STATE),
       scan((state: TimerState, nextEvent: Partial<TimerState>): TimerState => ({ ...state, ...nextEvent })),
       switchMap((state: TimerState) =>
         state.running
-          ? timer(0, state.tickSpeed).pipe(
-              tap(() => {
-                if (state.currentTime.equals(state.endTime)) {
-                  this.setRunning(false);
-                } else {
-                  state.currentTime = state.currentTime.next();
-                }
-              }),
-              mapTo(state),
-            )
-          : of(state)
+          ? concat(
+              of(state),
+              interval(state.tickSpeed).pipe(
+                tap(() => {
+                  if (state.currentTime.equals(state.endTime)) {
+                    this.setRunning(false);
+                  } else {
+                    state.currentTime = state.currentTime.next();
+                  }
+                }),
+                mapTo(state),
+              )
+          ) : of(state)
       ),
       share()
     );
