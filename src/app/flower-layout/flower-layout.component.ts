1dc5174319526a6da15c1b3c15dcfa2f52b09c33
Paul Friederichsen <floogulinc@gmail.com>
2020-06-10 02:04:04 -0500

    Add demo flowers and scales from original component

--- src/app/flower-layout/flower-layout.component.ts
+++ src/app/flower-layout/flower-layout.component.ts
@@ -10,37 +10,19 @@ export class FlowerLayoutComponent implements OnInit {
 
   flowers: FlowerLayoutItem[] = [
     {
-      imgSrc: 'assets/images/1000w-8bit/flowers/sunflower.png',
+      imgSrc: 'assets/images/1000w-8bit/flowers/rudbeckia hirta.png',
       alt: 'test',
       active: true,
-      scale: 1
+      scale: 1.9
     },
     {
-      imgSrc: 'assets/images/1000w-8bit/flowers/sunflower.png',
+      imgSrc: 'assets/images/1000w-8bit/flowers/taraxacum officinale.png',
       alt: 'test',
       active: true,
-      scale: 0.7
+      scale: 1.1
     },
     {
-      imgSrc: 'assets/images/1000w-8bit/flowers/sunflower.png',
-      alt: 'test',
-      active: true,
-      scale: 1.2
-    },
-    {
-      imgSrc: 'assets/images/1000w-8bit/flowers/sunflower.png',
-      alt: 'test',
-      active: true,
-      scale: 1
-    },
-    {
-      imgSrc: 'assets/images/1000w-8bit/flowers/sunflower.png',
-      alt: 'test',
-      active: true,
-      scale: 0.7
-    },
-    {
-      imgSrc: 'assets/images/1000w-8bit/flowers/sunflower.png',
+      imgSrc: 'assets/images/1000w-8bit/flowers/solidago rigida.png',
       alt: 'test',
       active: true,
       scale: 1.3
@@ -49,7 +31,43 @@ export class FlowerLayoutComponent implements OnInit {
       imgSrc: 'assets/images/1000w-8bit/flowers/sunflower.png',
       alt: 'test',
       active: true,
-      scale: 0.5
+      scale: 1.5
+    },
+    {
+      imgSrc: 'assets/images/1000w-8bit/flowers/black raspberry.png',
+      alt: 'test',
+      active: true,
+      scale: 1
+    },
+    {
+      imgSrc: 'assets/images/1000w-8bit/flowers/trifolium repens.png',
+      alt: 'test',
+      active: true,
+      scale: 1.1
+    },
+    {
+      imgSrc: 'assets/images/1000w-8bit/flowers/vaccinium angustifolium.png',
+      alt: 'test',
+      active: true,
+      scale: 1.5
+    },
+    {
+      imgSrc: 'assets/images/1000w-8bit/flowers/rudbeckia hirta.png',
+      alt: 'test',
+      active: true,
+      scale: 2.1
+    },
+    {
+      imgSrc: 'assets/images/1000w-8bit/flowers/taraxacum officinale.png',
+      alt: 'test',
+      active: true,
+      scale: 1.1
+    },
+    {
+      imgSrc: 'assets/images/1000w-8bit/flowers/solidago rigida.png',
+      alt: 'test',
+      active: true,
+      scale: 1.3
     },
     {
       imgSrc: 'assets/images/1000w-8bit/flowers/sunflower.png',
@@ -58,52 +76,34 @@ export class FlowerLayoutComponent implements OnInit {
       scale: 1.5
     },
     {
-      imgSrc: 'assets/images/1000w-8bit/flowers/sunflower.png',
+      imgSrc: 'assets/images/1000w-8bit/flowers/black raspberry.png',
       alt: 'test',
       active: true,
-      scale: 1
+      scale: 0.9
+    },
+    {
+      imgSrc: 'assets/images/1000w-8bit/flowers/trifolium repens.png',
+      alt: 'test',
+      active: true,
+      scale: 1.1
+    },
+    {
+      imgSrc: 'assets/images/1000w-8bit/flowers/vaccinium angustifolium.png',
+      alt: 'test',
+      active: true,
+      scale: 1.5
     },
     {
       imgSrc: 'assets/images/1000w-8bit/flowers/sunflower.png',
       alt: 'test',
       active: true,
-      scale: 1
+      scale: 1.5
     },
     {
-      imgSrc: 'assets/images/1000w-8bit/flowers/sunflower.png',
+      imgSrc: 'assets/images/1000w-8bit/flowers/black raspberry.png',
       alt: 'test',
       active: true,
-      scale: 1
-    },
-    {
-      imgSrc: 'assets/images/1000w-8bit/flowers/sunflower.png',
-      alt: 'test',
-      active: true,
-      scale: 1
-    },
-    {
-      imgSrc: 'assets/images/1000w-8bit/flowers/sunflower.png',
-      alt: 'test',
-      active: true,
-      scale: 1
-    },
-    {
-      imgSrc: 'assets/images/1000w-8bit/flowers/sunflower.png',
-      alt: 'test',
-      active: true,
-      scale: 1
-    },
-    {
-      imgSrc: 'assets/images/1000w-8bit/flowers/sunflower.png',
-      alt: 'test',
-      active: true,
-      scale: 1
-    },
-    {
-      imgSrc: 'assets/images/1000w-8bit/flowers/sunflower.png',
-      alt: 'test',
-      active: true,
-      scale: 1
+      scale: 0.9
     }
   ]
 
