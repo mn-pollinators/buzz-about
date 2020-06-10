795a29c554a465017caff6320f161556924eab72
Paul Friederichsen <floogulinc@gmail.com>
2020-06-10 02:11:12 -0500

    Remove old value and function from FlowerLayoutItem

--- src/app/flower-layout-item/flower-layout-item.component.ts
+++ src/app/flower-layout-item/flower-layout-item.component.ts
@@ -32,31 +32,9 @@ export interface FlowerLayoutItem {
 })
 export class FlowerLayoutItemComponent implements OnInit, OnChanges {
 
-  /**
-   * Each flower has a `scale` value, saying how large they are relative to an
-   * average flower. (Your average flower has a scale of 1.)
-   *
-   * This value here says, "Take your average flower. How wide should that
-   * flower be, in units of percent-of-the-screen-width?"
-   *
-   * Then, other flowers will have their widths calculated based on this value.
-   */
-  static readonly averageFlowerWidthPercent = 15;
-
   @Input() item: FlowerLayoutItem;
 
   constructor() { }
-
-
-  // TODO getWidth() computes width from flower scale and averageFlowerWidth
-  // TODO look at Angular Animations, do we need to pass in variables in the template or can we set them in the animations stuff above?
-  // Can we just call a function to trigger the change?
-  // We need to make sure that when the x/y input changes we reposition the item.
-
-  get currentFlowerWidthPercent(): number {
-    return FlowerLayoutItemComponent.averageFlowerWidthPercent * this.item.scale;
-  }
-
 
   ngOnInit() {
   }
