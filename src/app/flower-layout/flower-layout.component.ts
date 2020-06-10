c07e34f7b87ad136763b6ef74c7b83bcc7f7e9ba
Paul Friederichsen <floogulinc@gmail.com>
2020-06-10 02:55:08 -0500

    Move test images out into flower-test component and add test controls

--- src/app/flower-layout/flower-layout.component.ts
+++ src/app/flower-layout/flower-layout.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { FlowerLayoutItem } from '../flower-layout-item/flower-layout-item.component';
 
 @Component({
@@ -8,104 +8,7 @@ import { FlowerLayoutItem } from '../flower-layout-item/flower-layout-item.compo
 })
 export class FlowerLayoutComponent implements OnInit {
 
-  flowers: FlowerLayoutItem[] = [
-    {
-      imgSrc: 'assets/images/1000w-8bit/flowers/rudbeckia hirta.png',
-      alt: 'test',
-      active: true,
-      scale: 1.9
-    },
-    {
-      imgSrc: 'assets/images/1000w-8bit/flowers/taraxacum officinale.png',
-      alt: 'test',
-      active: true,
-      scale: 1.1
-    },
-    {
-      imgSrc: 'assets/images/1000w-8bit/flowers/solidago rigida.png',
-      alt: 'test',
-      active: true,
-      scale: 1.3
-    },
-    {
-      imgSrc: 'assets/images/1000w-8bit/flowers/sunflower.png',
-      alt: 'test',
-      active: true,
-      scale: 1.5
-    },
-    {
-      imgSrc: 'assets/images/1000w-8bit/flowers/black raspberry.png',
-      alt: 'test',
-      active: true,
-      scale: 1
-    },
-    {
-      imgSrc: 'assets/images/1000w-8bit/flowers/trifolium repens.png',
-      alt: 'test',
-      active: true,
-      scale: 1.1
-    },
-    {
-      imgSrc: 'assets/images/1000w-8bit/flowers/vaccinium angustifolium.png',
-      alt: 'test',
-      active: true,
-      scale: 1.5
-    },
-    {
-      imgSrc: 'assets/images/1000w-8bit/flowers/rudbeckia hirta.png',
-      alt: 'test',
-      active: true,
-      scale: 2.1
-    },
-    {
-      imgSrc: 'assets/images/1000w-8bit/flowers/taraxacum officinale.png',
-      alt: 'test',
-      active: true,
-      scale: 1.1
-    },
-    {
-      imgSrc: 'assets/images/1000w-8bit/flowers/solidago rigida.png',
-      alt: 'test',
-      active: true,
-      scale: 1.3
-    },
-    {
-      imgSrc: 'assets/images/1000w-8bit/flowers/sunflower.png',
-      alt: 'test',
-      active: true,
-      scale: 1.5
-    },
-    {
-      imgSrc: 'assets/images/1000w-8bit/flowers/black raspberry.png',
-      alt: 'test',
-      active: true,
-      scale: 0.9
-    },
-    {
-      imgSrc: 'assets/images/1000w-8bit/flowers/trifolium repens.png',
-      alt: 'test',
-      active: true,
-      scale: 1.1
-    },
-    {
-      imgSrc: 'assets/images/1000w-8bit/flowers/vaccinium angustifolium.png',
-      alt: 'test',
-      active: true,
-      scale: 1.5
-    },
-    {
-      imgSrc: 'assets/images/1000w-8bit/flowers/sunflower.png',
-      alt: 'test',
-      active: true,
-      scale: 1.5
-    },
-    {
-      imgSrc: 'assets/images/1000w-8bit/flowers/black raspberry.png',
-      alt: 'test',
-      active: true,
-      scale: 0.9
-    }
-  ]
+  @Input() items: FlowerLayoutItem[] = [];
 
   constructor() { }
 
