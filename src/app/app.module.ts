35dd919928ea411783b8c3e1fa2e8b780c96413b
Paul Friederichsen <floogulinc@gmail.com>
2020-06-11 17:29:56 -0500

    Remove old unused components
    Co-authored-by: Joe Walbran <walbr037@morris.umn.edu>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,10 +27,7 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { ArViewComponent } from './ar-view/ar-view.component';
 import { PlayRoundComponent } from './play-round/play-round.component';
 import { LargeDisplayComponent } from './large-display/large-display.component';
-import { TimerBarComponent } from './timer-bar/timer-bar.component';
 import { FullscreenButtonComponent } from './fullscreen-button/fullscreen-button.component';
-import { DisplayItemComponent } from './display-item/display-item.component';
-import { GameProgressIndicatorComponent } from './game-progress-indicator/game-progress-indicator.component';
 import { GameReviewPageComponent } from './game-review-page/game-review-page.component';
 import { ReviewItemComponent } from './review-item/review-item.component';
 import { ReviewPathComponent } from './review-path/review-path.component';
@@ -62,10 +59,7 @@ const MDC_MODULES: any[] = [
     ArViewComponent,
     PlayRoundComponent,
     LargeDisplayComponent,
-    TimerBarComponent,
     FullscreenButtonComponent,
-    DisplayItemComponent,
-    GameProgressIndicatorComponent,
     GameReviewPageComponent,
     ReviewItemComponent,
     ReviewPathComponent,
