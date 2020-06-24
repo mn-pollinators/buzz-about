3927b19c38e8010be04f98bd8349a8cae04dd6f1
Joe Walbran <walbr037@morris.umn.edu>
2020-06-24 16:01:37 -0500

    Make sure that Nest objects have an `id` field
    Like Bee and Flower objects.
    
    This id field is a unique identifier for that type of nest; it's the key by which that Nest can be accessed in the global `nests` dictionary.
    
    Technically speaking, you can always just get a Nest's id by doing a reverse lookup in the `nests` dictionary. That being said, we thought it would be helpful to denormalize the data a bit so you don't have to go through all that trouble.

--- src/app/nests.ts
+++ src/app/nests.ts
@@ -1,6 +1,7 @@
 import { nests as nestsFromJson } from '@mn-pollinators/assets/nests.json';
 
 export interface Nest {
+  id: string;
   name: string;
   art_file: string;
   photo_files: {
@@ -12,5 +13,14 @@ export interface Nest {
   description: string;
 }
 
-// Make sure that the values are actually Nests.
-export const nests: {[id in keyof typeof nestsFromJson]: Nest} = nestsFromJson;
+const nestsConverted: {[id: string]: Nest} = {};
+
+for (const [key, nestFromJson] of Object.entries(nestsFromJson)) {
+  nestsConverted[key] = {
+    ...nestFromJson,
+    id: key,
+  };
+}
+
+export const nests =
+  nestsConverted as {[id in keyof typeof nestsFromJson]: Nest};
