913f21a54ebb14fcad50e68629424e3f9bca591f
Joe Walbran <walbr037@morris.umn.edu>
2020-06-28 12:59:16 -0500

    Don't bother testing SessionTestComponent
    It's not going to be in production anyway; it's just for playing around with the Student*Services.

--- src/app/session-test/session-test.component.spec.ts
+++ src/app/session-test/session-test.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SessionTestComponent } from './session-test.component';
 
-describe('SessionTestComponent', () => {
+xdescribe('SessionTestComponent', () => {
   let component: SessionTestComponent;
   let fixture: ComponentFixture<SessionTestComponent>;
 
