cedfb2896e3608177db2ae9febd7072c09ddc346
Audrey Le Meur <lemeu001@umn.edu>
2020-06-30 12:29:30 -0500

    Create login form logic
    Begin writing component logic for login page. Addresses issue #64.
    
    Co-authored-by: Erik Rauer <rauer007@morris.umn.edu>

--- src/app/student-login/student-login.component.ts
+++ src/app/student-login/student-login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { FirebaseService } from '../firebase.service';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-student-login',
@@ -10,18 +11,25 @@ import { FirebaseService } from '../firebase.service';
 export class StudentLoginComponent implements OnInit {
 
   userID: string;
+  nameControl: FormControl;
+  sessionControl: FormControl;
+  sessionFormGroup: FormGroup;
 
   constructor(public authService: AuthService, public firebaseService: FirebaseService) {  }
 
   ngOnInit(): void {
     this.authService.logStudentIn();
+    this.nameControl = new FormControl();
+    this.sessionControl = new FormControl();
+    this.sessionFormGroup = new FormGroup({nameControl: this.nameControl, sessionControl: this.sessionControl});
   }
 
   /**
    * Calls firebase service to add currently logged in user and their preferred name to the database
-   * @param name Student's name
    */
-  addStudentToDatabase(name) {
+  addStudentToDatabase() {
+    const name = this.sessionFormGroup.controls.nameControl.value;
+    const session = this.sessionFormGroup.controls.sessionControl.value;
     this.authService.getCurrentUser$.subscribe( user => {
       this.userID = user.uid;
       this.firebaseService.addStudent(this.userID, {name});
