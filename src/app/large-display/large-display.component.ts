54887128c1d80e8c4625fbfb8ba035b2e2438c18
Audrey Le Meur <lemeu001@umn.edu>
2020-07-09 15:48:04 -0500

    Change bottom bar to component
    Made a bottom bar component and switched it out in the large display. 
    
    Addresses issue #75.
    
    Co-authored-by: Shawn Reuter <reute078@morris.umn.edu>

--- src/app/large-display/large-display.component.ts
+++ src/app/large-display/large-display.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { FlowerLayoutItem } from '../flower-layout-item/flower-layout-item.component';
+import { BottomBarComponent } from '../bottom-bar/bottom-bar.component';
 import { TimerService } from '../timer.service';
 import { TimePeriod } from '../time-period';
 
