ea9265e352ef267f00f5eaa036cebf9cbee83f48
Erik <eriktr22@gmail.com>
2020-07-14 15:47:17 -0500

    Change Session Lobby to use demo-session

--- src/app/session-lobby/session-lobby.component.ts
+++ src/app/session-lobby/session-lobby.component.ts
@@ -11,11 +11,11 @@ export class SessionLobbyComponent implements OnInit {
   sessionID: string;
 
   constructor(public teacherSessionService: TeacherSessionService) {
-    this.sessionID = 'kugTpWqJyrXaJZ4ZB6zE'; // Temporary until a way to get the session is implemented
+    this.sessionID = 'demo-session'; // Temporary until a way to get the session is implemented
   }
 
   ngOnInit(): void {
-    // Temporary, teacher will likely join session immediately after creating it in
+    // Temporary, teacher will likely join session immediately after creating it
     this.teacherSessionService.joinSession(this.sessionID);
   }
 }
