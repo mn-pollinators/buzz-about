b05a6d015bcbbf2068da6b87712ac9a64d053b0b
Paul Friederichsen <floogulinc@gmail.com>
2020-07-24 15:06:05 -0500

    Remove old components from routes and app module
    Co-authored-by: Joe Walbran <walbr037@morris.umn.edu>
    Co-authored-by: Utkarsh Kumar <kumar375@morris.umn.edu>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,6 @@ import { LargeDisplayComponent } from './large-display/large-display.component';
 import { TimerTestComponent } from './timer-test/timer-test.component';
 import { FlowerTestComponent } from './flower-test/flower-test.component';
 import { HomeComponent } from './home/home.component';
-import { FirebaseTestComponent } from './firebase-test/firebase-test.component';
-import { SessionPageTestComponent } from './session-page-test/session-page-test.component';
 import { JsonDataTestComponent } from './json-data-test/json-data-test.component';
 import { SessionTestComponent } from './session-test/session-test.component';
 import { StudentLoginComponent } from './student-login/student-login.component';
@@ -17,11 +15,8 @@ const routes: Routes = [
   {path: 'ar', component: PlayRoundComponent},
   {path: 'teacher', component: LargeDisplayComponent},
   {path: 'timer-test', component: TimerTestComponent},
-  {path: 'flower-test', component: FlowerTestComponent},
-  {path: 'firebase-test', component: FirebaseTestComponent},
   {path: 'json-data-test', component: JsonDataTestComponent},
   {path: 'session-test', component: SessionTestComponent},
-  {path: 'session-page-test', component: SessionPageTestComponent},
   {path: 'json-data-test', component: JsonDataTestComponent},
   {path: 'session-lobby', component: SessionLobbyComponent},
   {path: 'join', component: StudentLoginComponent}
