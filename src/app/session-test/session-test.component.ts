4bdb4e603d12b8ce4de3abf4cae5a16fa30e0e8d
Paul Friederichsen <floogulinc@gmail.com>
2020-07-31 16:17:47 -0500

    Don't attempt to join a session on init in the session test component

--- src/app/session-test/session-test.component.ts
+++ src/app/session-test/session-test.component.ts
@@ -25,7 +25,6 @@ export class SessionTestComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.sessionService.joinSession(this.sessionId);
   }
 
   joinSession(sessionId: string, name: string) {
