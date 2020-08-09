f5b5deebd092a7abee3e7192a20c9449532c45a1
Paul Friederichsen <floogulinc@gmail.com>
2020-08-09 02:55:06 -0500

    Add basic demo of generating barcodes

--- src/app/pages/marker-generator/marker-generator.component.ts
+++ src/app/pages/marker-generator/marker-generator.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BarcodeMarkerGenerator, MATRIX_5X5_BCH_2277 } from 'studio-backend/src/modules/marker/tools/barcode-marker-generator';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-marker-generator',
@@ -8,11 +9,20 @@ import { BarcodeMarkerGenerator, MATRIX_5X5_BCH_2277 } from 'studio-backend/src/
 })
 export class MarkerGeneratorComponent implements OnInit {
 
-  constructor() { }
+  constructor(public sanitizer: DomSanitizer) { }
+
+  test: BarcodeMarkerGenerator;
+
+  barcodeValue = '0';
+
+  getBarcode(value: string) {
+    const marker = new BarcodeMarkerGenerator(MATRIX_5X5_BCH_2277, parseInt(value, 10));
+    return this.sanitizer.bypassSecurityTrustUrl(marker.asSVGDataURI());
+  }
 
   ngOnInit(): void {
-    const test = new BarcodeMarkerGenerator(MATRIX_5X5_BCH_2277, 1);
-    console.log(test.asSVG());
+    this.test = new BarcodeMarkerGenerator(MATRIX_5X5_BCH_2277, 1);
+    console.log(this.test.asSVG());
     console.log(BarcodeMarkerGenerator.getMatrixTypes());
   }
 
