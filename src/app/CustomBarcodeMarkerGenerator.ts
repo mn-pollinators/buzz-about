318031a5df18cca3289f7123be30c03d3206c9c4
Paul Friederichsen <floogulinc@gmail.com>
2020-08-09 04:58:32 -0500

    Write custom version of the barcode generator that allows for custom sizes of the SVG

---
+++ src/app/CustomBarcodeMarkerGenerator.ts
@@ -0,0 +1,36 @@
+
+import { BarcodeMarkerGenerator } from 'studio-backend/src/modules/marker/tools/barcode-marker-generator';
+
+// Code adapted from https://github.com/AR-js-org/studio-backend/blob/master/src/modules/marker/tools/barcode-marker-generator.js
+
+export class CustomBarcodeMarkerGenerator extends BarcodeMarkerGenerator {
+  asSVGWithSize(size: number) {
+    let svgStr =
+      '<?xml version="1.0" encoding="utf-8"?>' +
+      `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="${size}" height="${size}">` +
+      `<rect x="0" y="0" width="${size}" height="${size}" fill="black" />`;
+
+    const pixelSize = (size / 2) / this.typeDesc.matrixSize;
+    for (let y = 0; y < this.typeDesc.matrixSize; ++y) {
+      for (let x = 0; x < this.typeDesc.matrixSize; ++x) {
+        if (!this.valueEncoded[y * this.typeDesc.matrixSize + x]) {
+          svgStr += `<rect x="${(size / 4) + x * pixelSize}" y="${(size / 4) +
+            y *
+            pixelSize}" width="${pixelSize}" height="${pixelSize}" fill="white" />`;
+        }
+      }
+    }
+
+    svgStr += '</svg>';
+    return svgStr;
+  }
+
+  asSVGDataURIWithSize(size: number) {
+    return (
+      `data:image/svg+xml,${
+      encodeURIComponent(this.asSVGWithSize(size))
+        .replace(/'/g, '%27')
+        .replace(/"/g, '%22')}`
+    );
+  }
+}
