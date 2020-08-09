c1571f1821c75ce832808bcf90983d59581fe5a6
Utkarsh Kumar <kumar375@morris.umn.edu>
2020-08-09 01:57:30 -0500

    Change wording & acceptable nest ID range: 18-127.

--- src/app/pages/join-session/join-session.component.ts
+++ src/app/pages/join-session/join-session.component.ts
@@ -15,7 +15,7 @@ export class JoinSessionComponent implements OnInit {
   sessionFormGroup = new FormGroup({
     nameControl: new FormControl('', Validators.required),
     sessionControl: new FormControl('', Validators.required),
-    nestControl: new FormControl('', [Validators.required, Validators.min(20), Validators.max(100),
+    nestControl: new FormControl('', [Validators.required, Validators.min(18), Validators.max(127),
       Validators.pattern('^[0-9]*$')])
   });
 
