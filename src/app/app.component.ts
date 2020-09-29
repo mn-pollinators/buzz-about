d2c7900792adc81a23caeed091f0b5e554879475
Paul Friederichsen <floogulinc@gmail.com>
2020-09-29 07:15:01 -0500

    Fix tslint issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { environment } from '../environments/environment';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
 
   constructor() {}
 
