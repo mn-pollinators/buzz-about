28652af2fd26fae07cb997e555cc2288e5ae57e0
Paul Friederichsen <floogulinc@gmail.com>
2020-09-29 07:02:04 -0500

    Add prod vs dev build to console logs at app start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { buzzAbout as buzzAboutInfo, assets as assetsInfo } from '../../project-info.json';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -13,6 +14,7 @@ export class AppComponent implements OnInit{
   ngOnInit() {
     console.log(`Buzz About v${buzzAboutInfo.version} (Assets v${assetsInfo.version}) loaded.`);
     console.log(`Commit hash: ${buzzAboutInfo.git.hash}`);
+    console.log(`This is a ${environment.production ? 'production' : 'development'} build.`);
   }
 
 }
