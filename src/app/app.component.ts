8e2a3a68c3db13e56a0139e43bdf46fbd6fc2737
Paul Friederichsen <floogulinc@gmail.com>
2020-09-29 06:56:17 -0500

    Print version info to console when app starts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { buzzAbout as buzzAboutInfo, assets as assetsInfo } from '../../project-info.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  constructor() {}
+
+  ngOnInit() {
+    console.log(`Buzz About v${buzzAboutInfo.version} (Assets v${assetsInfo.version}) loaded.`);
+    console.log(`Commit hash: ${buzzAboutInfo.git.hash}`);
+  }
+
 }
