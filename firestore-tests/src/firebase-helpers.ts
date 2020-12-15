e59557d4a1e294519d347ff5c4fdbb1f05838c4c
Paul Friederichsen <floogulinc@gmail.com>
2020-12-14 18:47:18 -0600

    Move milliseconds function to util file
    Co-authored-by: Joe Walbran <walbr037@morris.umn.edu>

--- firestore-tests/src/firebase-helpers.ts
+++ firestore-tests/src/firebase-helpers.ts
@@ -133,10 +133,7 @@ export function createJoinCode(db: firestore.Firestore, id: string, joinCodeData
   return db.collection('joinCodes').doc(id).set(joinCodeData);
 }
 
-// https://stackoverflow.com/a/41297064/8855259
-export function milliseconds(hrs: number, min: number, sec: number): number {
-  return((hrs * 60 * 60 + min * 60 + sec) * 1000);
-}
+export {milliseconds} from '../../src/app/utils/time-utils';
 
 export async function queryJoinCodesBySessionId(db: firestore.Firestore, sessionId: string) {
   return db.collection('joinCodes').where('sessionId', '==', sessionId).get();
