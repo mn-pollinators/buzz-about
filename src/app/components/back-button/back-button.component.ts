821c0bb1db71aab08606025ffb9125e4a8ee24e2
Paul Friederichsen <floogulinc@gmail.com>
2021-01-08 13:30:53 -0600

    Move routerLink out of back button component
    This avoids having to import the RouterModule in our SharedModule and makes the target of the back button obvious wherever its used.

--- src/app/components/back-button/back-button.component.ts
+++ src/app/components/back-button/back-button.component.ts
@@ -7,8 +7,6 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class BackButtonComponent implements OnInit {
 
-  @Input() backRouterLink: any[] | string = '/';
-
   constructor() { }
 
   ngOnInit(): void {
