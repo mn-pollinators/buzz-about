5c510fe4997cb690056d99030b71d467a4804c69
Utkarsh Kumar <kumar375@morris.umn.edu>
2021-01-11 03:28:10 -0600

    Detect Markers representing Nests

--- src/app/components/scan-nest/scan-nest.component.ts
+++ src/app/components/scan-nest/scan-nest.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { distinctUntilChanged, map, shareReplay } from 'rxjs/operators';
+import { MarkerState } from '../ar-view/ar-view.component';
+import { MAX_NEST_MARKER, MIN_NEST_MARKER } from 'src/app/markers';
 
 @Component({
   selector: 'app-scan-nest',
@@ -8,6 +12,31 @@ import { Component, OnInit } from '@angular/core';
 export class ScanNestComponent implements OnInit {
 
   constructor() { }
+
+  currentMarkerStates$ = new BehaviorSubject<MarkerState[]>([]);
+
+  foundMarkerValue$: Observable<number | null> = this.currentMarkerStates$.pipe(
+    map(markers => markers.filter(m => m.found)),
+    map(markers =>
+      markers.length > 0
+        ? markers.reduce((prev, curr) =>
+          prev.distance < curr.distance ? prev : curr
+        ).barcodeValue
+        : null
+    ),
+    distinctUntilChanged(),
+    shareReplay(1)
+  );
+
+  foundNestMarker$: Observable<boolean | null>  = this.foundMarkerValue$.pipe(
+    map(val => val ? val >= MIN_NEST_MARKER && val <= MAX_NEST_MARKER : null),
+    distinctUntilChanged(),
+    shareReplay(1)
+  );
+
+  onMarkerState(states: MarkerState[]) {
+    this.currentMarkerStates$.next(states);
+  }
 
   ngOnInit(): void {
   }
