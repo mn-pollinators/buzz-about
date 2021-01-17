68d3a94366e7db7dde88e62339f4a616187d1464
Paul Friederichsen <floogulinc@gmail.com>
2021-01-17 09:13:23 -0600

    Add info about current user to admin homepage

--- src/app/admin-pages/admin-home/admin-home.component.ts
+++ src/app/admin-pages/admin-home/admin-home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AdminService } from 'src/app/services/admin.service';
 
 @Component({
   selector: 'app-admin-home',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminHomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(public adminService: AdminService) { }
 
   ngOnInit(): void {
   }
