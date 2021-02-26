d8ee4403200919738bdd1d638bcef2c14df0d7a6
Paul Friederichsen <floogulinc@gmail.com>
2021-02-26 07:27:42 -0600

    Remove old comment and unused imports from FullScreenButton component

--- src/app/components/fullscreen-button/fullscreen-button.component.ts
+++ src/app/components/fullscreen-button/fullscreen-button.component.ts
@@ -1,11 +1,6 @@
-import { Component, Inject, OnInit, HostListener } from '@angular/core';
-import { DOCUMENT } from '@angular/common';
-import { UntilDestroy } from '@ngneat/until-destroy';
-import { fromEvent } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
 import { ScreenfullService } from '@ngx-extensions/screenfull';
 
-// Code from this stackoverflow issue:
-// https://stackoverflow.com/questions/51998594/how-to-make-google-chrome-go-full-screen-in-angular-4-application
 @Component({
   selector: 'app-fullscreen-button',
   templateUrl: './fullscreen-button.component.html',
