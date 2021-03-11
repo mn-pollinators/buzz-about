e8293f9bac3af875a2ba7ec8f4ee33058f88bf7e
Paul Friederichsen <floogulinc@gmail.com>
2021-03-10 18:57:46 -0600

    Allow for multiple options for month text and attempt to fix offsets

--- src/app/components/small-timeline/small-timeline.component.ts
+++ src/app/components/small-timeline/small-timeline.component.ts
@@ -1,5 +1,7 @@
 import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 import { MAX_TIME, Month, TimePeriod } from 'src/app/time-period';
+import { rangeArray } from 'src/app/utils/array-utils';
+
 
 @Component({
   selector: 'app-small-timeline',
@@ -10,9 +12,24 @@ import { MAX_TIME, Month, TimePeriod } from 'src/app/time-period';
 export class SmallTimelineComponent implements OnInit {
 
   @Input() timeRange: [TimePeriod, TimePeriod];
-  @Input() showText = true;
+  @Input() showMonths?: 'startAndEnd' | 'inRange' | 'all';
 
   MAX_TIME = MAX_TIME;
+
+  Month = Month;
+
+  getMonths(): Month[] {
+    switch (this.showMonths) {
+      case 'startAndEnd':
+        return [this.timeRange[0].month, this.timeRange[1].month];
+      case 'inRange':
+        return rangeArray(this.timeRange[0].month, this.timeRange[1].month);
+      case 'all':
+        return rangeArray(Month.January, Month.December);
+      default:
+        return [];
+    }
+  }
 
   constructor() { }
 
@@ -21,15 +38,15 @@ export class SmallTimelineComponent implements OnInit {
 
   getInnerBarStyle() {
     return {
-      left: `${(this.timeRange[0].time / MAX_TIME) * 100}%`,
-      right: `${100 - (this.timeRange[1].time / MAX_TIME) * 100}%`
+      left: `${((this.timeRange[0].time) / (MAX_TIME + 1)) * 100}%`,
+      right: `${100 - ((this.timeRange[1].time + 1) / (MAX_TIME + 1)) * 100}%`
     };
   }
 
   getTextStyle(month: Month) {
     const middleOfMonth = TimePeriod.fromMonthAndQuarter(month, 3);
     return {
-      left: `${(middleOfMonth.time / MAX_TIME) * 100}%`,
+      left: `${((middleOfMonth.time) / (MAX_TIME + 1)) * 100}%`,
     };
   }
 
--- src/app/test-pages/field-guide-test/field-guide-test.component.ts
+++ src/app/test-pages/field-guide-test/field-guide-test.component.ts
@@ -3,6 +3,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { allBeeSpecies } from 'src/app/bees';
 import { FieldGuideDialogComponent, FieldGuideDialogData } from 'src/app/components/field-guide-dialog/field-guide-dialog.component';
 import { allFlowerSpecies } from 'src/app/flowers';
+import { MAX_TIME, MAX_TIME_PERIOD, TimePeriod } from 'src/app/time-period';
 
 @Component({
   selector: 'app-field-guide-test',
@@ -15,6 +16,18 @@ export class FieldGuideTestComponent implements OnInit {
 
   flowers = Object.values(allFlowerSpecies);
   bees = Object.values(allBeeSpecies);
+
+  testTimes = [
+    [new TimePeriod(0), MAX_TIME_PERIOD],
+    [new TimePeriod(1), MAX_TIME_PERIOD],
+    [new TimePeriod(0), new TimePeriod(MAX_TIME - 1)],
+    [new TimePeriod(0), new TimePeriod(3)],
+    [new TimePeriod(1), new TimePeriod(2)],
+    [new TimePeriod(2), new TimePeriod(3)],
+    [TimePeriod.fromMonthAndQuarter(12, 1), TimePeriod.fromMonthAndQuarter(12, 4)],
+    [new TimePeriod(0), new TimePeriod(4)],
+    [new TimePeriod(2), new TimePeriod(2)]
+  ]
 
   ngOnInit(): void {
     // this.openDialog({type: 'flower', value: allFlowerSpecies.asclepias_syriaca});
