d640614996b9fac2926ee7b8f3959f81d2b3e8cd
Paul Friederichsen <floogulinc@gmail.com>
2021-04-20 16:09:00 -0500

    Clean up code

--- src/app/components/ar-view/ar-view.component.ts
+++ src/app/components/ar-view/ar-view.component.ts
@@ -64,15 +64,12 @@ export class ArViewComponent implements OnInit, AfterViewInit, OnChanges, OnDest
   scene: THREE.Scene; // The Three scene, where the groups to display on markers are held
   camera: THREE.Camera;
   renderer: THREE.WebGLRenderer;
-  clock: THREE.Clock;
-  deltaTime = 0;
-  totalTime = 0;
   source: THREEAR.Source;
 
   controller: THREEAR.Controller;
 
   arReady = false;
-  disposeQueued = false;
+  disposed = false;
 
   lastActiveMarker: number;
 
@@ -83,7 +80,6 @@ export class ArViewComponent implements OnInit, AfterViewInit, OnChanges, OnDest
 
   // Provides a reference for the canvas element
   private get canvas(): HTMLCanvasElement {
-    console.log('get canvas');
     return this.canvasRef.nativeElement;
   }
 
@@ -93,7 +89,6 @@ export class ArViewComponent implements OnInit, AfterViewInit, OnChanges, OnDest
 
   // Provide a reference for the container
   private get container(): HTMLDivElement {
-    console.log('get container');
     return this.containerRef.nativeElement;
   }
 
@@ -158,24 +153,23 @@ export class ArViewComponent implements OnInit, AfterViewInit, OnChanges, OnDest
   ngOnInit() {
   }
 
-  ngAfterViewInit() {
-    this.initAR().then(() => { // init ar
+  async ngAfterViewInit() {
+    await this.initAR(); // init ar
 
-      if (this.disposeQueued) {
-        return;
-      }
+    if (this.disposed) {
+      return;
+    }
 
-      // Add initial set of markers passed into the component
-      if (this.markers) {
-        this.markers.forEach((marker) => this.addMarker(marker));
-      }
+    // Add initial set of markers passed into the component
+    if (this.markers) {
+      this.markers.forEach((marker) => this.addMarker(marker));
+    }
 
-      // Setup AR events
-      this.setupEvents();
+    // Setup AR events
+    this.setupEvents();
 
-      // Set flag that AR is ready
-      this.arReady = true;
-    });
+    // Set flag that AR is ready
+    this.arReady = true;
   }
 
 
@@ -183,105 +177,84 @@ export class ArViewComponent implements OnInit, AfterViewInit, OnChanges, OnDest
    * Setup the canvas, camera, and basic AR library
    * @returns a promise that resolves when AR is ready.
    */
-  private initAR(): Promise<any> {
-    console.log('initAR called');
-    return new Promise<void>((resolve, reject) => {
-      console.log('initAR promise start');
-      this.clock = new THREE.Clock(); // setup the clock for keeping track of frametimes
+  private async initAR() {
+    this.scene = new THREE.Scene(); // create the main scene
 
-      this.scene = new THREE.Scene(); // create the main scene
+    // Set lighting
+    const ambientLight = new THREE.AmbientLight(0xcccccc, 0.5);
+    this.scene.add(ambientLight);
 
-      // Set lighting
-      const ambientLight = new THREE.AmbientLight(0xcccccc, 0.5);
-      this.scene.add(ambientLight);
+    // Setup camera
+    this.camera = new THREE.Camera();
+    this.scene.add(this.camera);
 
-      // Setup camera
-      this.camera = new THREE.Camera();
-      this.scene.add(this.camera);
-
-      // Setup renderer
-      this.renderer = new THREE.WebGLRenderer({
-        antialias: true,
-        alpha: true,
-        canvas: this.canvas // Point the renderer at the canvas element in the HTML
-      });
-
-      this.renderer.setClearColor(new THREE.Color('lightgrey'), 0);
-      this.renderer.setSize(ArResolution.width, ArResolution.height); // TODO: resize to entire component size
-
-      // Setup the THREEAR Source with the parent div and the renderer and camera we setup
-      this.source = new THREEAR.Source({
-        parent: this.container,
-        renderer: this.renderer,
-        camera: this.camera,
-        sourceWidth: ArResolution.width,
-        sourceHeight: ArResolution.height,
-        displayWidth: ArResolution.width,
-        displayHeight: ArResolution.height,
-      });
-
-      console.log('before THREEAR init');
-      // Initialize THREEAR
-      THREEAR.initialize(
-        {
-          source: this.source,
-          // the mode of detection - ['color', 'color_and_matrix', 'mono', 'mono_and_matrix']
-          detectionMode: 'mono_and_matrix',
-
-          // type of matrix code - valid iif detectionMode end with 'matrix' -
-          // [3x3, 3x3_HAMMING63, 3x3_PARITY65, 4x4, 4x4_BCH_13_9_3, 4x4_BCH_13_5_5]
-          matrixCodeType: '4x4_BCH_13_9_3',
-
-          // timeout for setting a marker as lost when it is no longer seen
-          // default was 1000
-          lostTimeout: 100,
-
-          positioning: {
-            // turn on/off camera smoothing
-            smooth: false, // note: turning this off seems to make the tracking much better
-
-            // number of matrices to smooth tracking over, more = smoother but slower follow
-            smoothCount: 5,
-
-            // distance tolerance for smoothing, if smoothThreshold # of matrices are under tolerance, tracking will stay still
-            smoothTolerance: 0.01,
-
-            // threshold for smoothing, will keep still unless enough matrices are over tolerance
-            smoothThreshold: 2
-          },
-
-          // tune the maximum rate of pose detection in the source image
-          maxDetectionRate: 60,
-
-          // resolution of at which we detect pose in the source image
-          canvasWidth: ArResolution.width,
-          canvasHeight: ArResolution.height,
-
-          // the patternRatio inside the artoolkit marker - artoolkit only
-          patternRatio: 0.5,
-
-          // enable image smoothing or not for canvas copy - default to true
-          // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/imageSmoothingEnabled
-          imageSmoothingEnabled: true
-        }
-      ).then((controller: THREEAR.Controller) => {
-        // Initialize returns a controller, set this.controller to that so we have a reference to it
-        this.controller = controller;
-
-        console.log('before first request animation frame');
-        // Call animate for the first time
-        requestAnimationFrame(() => { this.animate(); });
-
-        // Resolve the initAR Promise
-        resolve();
-
-      }).catch(error => {
-        console.log('THREEAR init error');
-        console.log(error);
-        //this.dispose();
-        reject(error);
-      });
+    // Setup renderer
+    this.renderer = new THREE.WebGLRenderer({
+      antialias: true,
+      alpha: true,
+      canvas: this.canvas // Point the renderer at the canvas element in the HTML
     });
+
+    this.renderer.setClearColor(new THREE.Color('lightgrey'), 0);
+    this.renderer.setSize(ArResolution.width, ArResolution.height); // TODO: resize to entire component size
+
+    // Setup the THREEAR Source with the parent div and the renderer and camera we setup
+    this.source = new THREEAR.Source({
+      parent: this.container,
+      renderer: this.renderer,
+      camera: this.camera,
+      sourceWidth: ArResolution.width,
+      sourceHeight: ArResolution.height,
+      displayWidth: ArResolution.width,
+      displayHeight: ArResolution.height,
+    });
+
+    // Initialize THREEAR
+    this.controller = await THREEAR.initialize(
+      {
+        source: this.source,
+        // the mode of detection - ['color', 'color_and_matrix', 'mono', 'mono_and_matrix']
+        detectionMode: 'mono_and_matrix',
+
+        // type of matrix code - valid iif detectionMode end with 'matrix' -
+        // [3x3, 3x3_HAMMING63, 3x3_PARITY65, 4x4, 4x4_BCH_13_9_3, 4x4_BCH_13_5_5]
+        matrixCodeType: '4x4_BCH_13_9_3',
+
+        // timeout for setting a marker as lost when it is no longer seen
+        // default was 1000
+        lostTimeout: 100,
+
+        positioning: {
+          // turn on/off camera smoothing
+          smooth: false, // note: turning this off seems to make the tracking much better
+
+          // number of matrices to smooth tracking over, more = smoother but slower follow
+          smoothCount: 5,
+
+          // distance tolerance for smoothing, if smoothThreshold # of matrices are under tolerance, tracking will stay still
+          smoothTolerance: 0.01,
+
+          // threshold for smoothing, will keep still unless enough matrices are over tolerance
+          smoothThreshold: 2
+        },
+
+        // tune the maximum rate of pose detection in the source image
+        maxDetectionRate: 60,
+
+        // resolution of at which we detect pose in the source image
+        canvasWidth: ArResolution.width,
+        canvasHeight: ArResolution.height,
+
+        // the patternRatio inside the artoolkit marker - artoolkit only
+        patternRatio: 0.5,
+
+        // enable image smoothing or not for canvas copy - default to true
+        // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/imageSmoothingEnabled
+        imageSmoothingEnabled: true
+      }
+    );
+
+    requestAnimationFrame(() => { this.animate(); });
   }
 
 
@@ -290,23 +263,14 @@ export class ArViewComponent implements OnInit, AfterViewInit, OnChanges, OnDest
    */
   private animate() {
     // Stop the animate loop when the controller has been disposed
-    if (this.disposeQueued) {
-      console.log('animate disposeQueued true');
-      //this.dispose();
+    if (this.disposed) {
       return;
     }
 
-    // Setup the next call
-    this.deltaTime = this.clock.getDelta(); // Calculate the time delta between frames
-    this.totalTime += this.deltaTime; // Add to the current timestamp
-
-    console.log('animate: before controller update');
     // Update the state of the THREEAR Controller
     this.controller.update(this.source.domElement);
-    console.log('animate: before render');
     // Render the markers
     this.renderer.render(this.scene, this.camera);
-    console.log('animate: before next frame');
     requestAnimationFrame(() => { this.animate(); });
   }
 
@@ -317,8 +281,6 @@ export class ArViewComponent implements OnInit, AfterViewInit, OnChanges, OnDest
    * @param imgPath the path to the image to display for the marker
    */
   public addMarker(marker: ARMarker): THREEAR.BarcodeMarker {
-    console.log(`addMarker called: ${marker.barcodeValue}`);
-
     // Setup the three group for this marker
     const markerGroup = new THREE.Group();
     // Add the group to the scene that contains all the markers
@@ -375,7 +337,6 @@ export class ArViewComponent implements OnInit, AfterViewInit, OnChanges, OnDest
   }
 
   private markerFoundOrLost() {
-    console.log('markerFoundOrLost called');
     const activeMarkers = this.controller.markers.barcode.filter(marker => marker.found);
 
     if (activeMarkers.length > 0) {
@@ -402,7 +363,6 @@ export class ArViewComponent implements OnInit, AfterViewInit, OnChanges, OnDest
    * Sets up marker events
    */
   private setupEvents() {
-    console.log('setupEvents called');
     this.controller.addEventListener('markerFound', () => {
       this.markerFoundOrLost();
     });
@@ -416,7 +376,6 @@ export class ArViewComponent implements OnInit, AfterViewInit, OnChanges, OnDest
    * @param changedMarkers the markers that have been changed and need to be updated or added
    */
   private updateMarkers(changedMarkers: ARMarker[]) {
-    console.log('updateMarkers called');
     // Loop through changed markers
     for (const changedMarker of changedMarkers) {
 
@@ -444,10 +403,8 @@ export class ArViewComponent implements OnInit, AfterViewInit, OnChanges, OnDest
   }
 
   ngOnChanges(changes: SimpleChanges) {
-    console.log('ngOnChanges called');
     // Check if there is a change to the markers and make sure AR has started
     if (typeof changes.markers !== 'undefined' && this.arReady) {
-      console.log('checking marker changes');
       const change = changes.markers;
 
       // We don't want to do this on the first change because that is when we are already setting the markers
@@ -496,14 +453,8 @@ export class ArViewComponent implements OnInit, AfterViewInit, OnChanges, OnDest
   }
 
   ngOnDestroy() {
-    console.log('ngOnDestroy called');
-    // Just set some flags. The next time a function like `animate()` or
-    // `updateMarkers` gets called, it'll check these flags and know to shut
-    // down.
-    this.disposeQueued = true;
-    this.arReady = false;
     this.dispose();
-    console.log('after this.dispose();');
+    this.arReady = false;
+    this.disposed = true;
   }
-
 }
