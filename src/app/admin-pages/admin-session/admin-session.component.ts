058856d46fe200e176de8ce50a140b94384f49a7
Paul Friederichsen <floogulinc@gmail.com>
2022-02-01 16:27:18 -0600

    Add the ability to download all data from a session

--- src/app/admin-pages/admin-session/admin-session.component.ts
+++ src/app/admin-pages/admin-session/admin-session.component.ts
@@ -1,23 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import { StudentRoundService } from 'src/app/services/student-round.service';
-import { StudentSessionService } from 'src/app/services/student-session.service';
-import { TeacherSessionService } from 'src/app/services/teacher-session.service';
-import { FirebaseService } from 'src/app/services/firebase.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AdminService } from 'src/app/services/admin.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-admin-session',
   templateUrl: './admin-session.component.html',
   styleUrls: ['./admin-session.component.scss']
 })
-export class AdminSessionComponent implements OnInit {
+export class AdminSessionComponent implements OnInit, OnDestroy {
 
   constructor(
-    public firebaseService: FirebaseService,
-    //public studentRoundService: StudentRoundService,
     public adminService: AdminService,
-    private activatedRoute: ActivatedRoute) { }
+    private activatedRoute: ActivatedRoute,
+    public matSnackbar: MatSnackBar
+  ) { }
 
   ngOnInit(): void {
     this.activatedRoute.paramMap.subscribe(params => {
@@ -28,4 +25,15 @@ export class AdminSessionComponent implements OnInit {
   ngOnDestroy() {
     this.adminService.leaveSession();
   }
+
+  downloadSession(sessionId: string) {
+    const snackBarRef = this.matSnackbar.open('Downloading session...');
+    this.adminService.downloadSessionJSON(sessionId).then(() => snackBarRef.dismiss(), error => {
+      snackBarRef.dismiss();
+      this.matSnackbar.open(`Error downloading: ${error.message}`, undefined, {
+        duration: 10000
+      });
+    });
+  }
+
 }
--- src/app/services/admin.service.ts
+++ src/app/services/admin.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestore } from '@angular/fire/firestore';
 import * as firebase from 'firebase/app';
-import { Observable, timer, BehaviorSubject, of } from 'rxjs';
-import { distinctUntilChanged, map, shareReplay, switchMap, tap } from 'rxjs/operators';
+import { Observable, timer, BehaviorSubject, of, combineLatest } from 'rxjs';
+import { distinctUntilChanged, map, shareReplay, switchMap, tap, switchMapTo } from 'rxjs/operators';
 import { JoinCode, JoinCodeWithId, JOIN_CODE_LIFESPAN } from '../join-code';
 import { Session, SessionWithId } from '../session';
 import { FirebaseService } from './firebase.service';
@@ -11,6 +11,7 @@ import firestore = firebase.firestore;
 import { StudentSessionService } from './student-session.service';
 import { TeacherSessionService } from './teacher-session.service';
 import { FirebaseRoundWithId, FirebaseRound } from '../round';
+import { downloadObjectToJSON } from '../utils/file-utils';
 
 @Injectable({
   providedIn: 'root'
@@ -78,6 +79,49 @@ export class AdminService {
     this.sessionId$.next(null);
     this.studentSessionService.leaveSession();
     this.teacherSessionService.leaveSession();
+  }
+
+  async getFullSessionData(sessionId: string) {
+    const sessionDoc = this.angularFirestore.firestore.collection('sessions').doc(sessionId);
+    const roundsCollection = sessionDoc.collection('rounds');
+
+    const sessionSnapshot = await sessionDoc.get();
+
+    const studentsSnapshot = await sessionDoc.collection('students').get();
+    const students = studentsSnapshot.docs.map(d => ({id: d.id, ...d.data()}));
+
+    const roundsSnapshot = await roundsCollection.get();
+
+    const rounds = await Promise.all(roundsSnapshot.docs.map(async roundSnapshot => {
+      const roundStudentsSnapshot = await roundSnapshot.ref.collection('students').get();
+      const roundStudents = roundStudentsSnapshot.docs.map(d => ({id: d.id, ...d.data()}));
+
+      const hostEventsSnapshot = await roundSnapshot.ref.collection('hostEvents').get();
+      const hostEvents = hostEventsSnapshot.docs.map(d => ({id: d.id, ...d.data()}));
+
+      const interactionsSnapshot = await roundSnapshot.ref.collection('interactions').get();
+      const interactions = interactionsSnapshot.docs.map(d => ({id: d.id, ...d.data()}));
+
+      return {
+        id: roundSnapshot.id,
+        ...roundSnapshot.data,
+        students: roundStudents,
+        hostEvents, interactions
+      };
+    }));
+
+    return {
+      id: sessionSnapshot.id,
+      ...sessionSnapshot.data(),
+      students,
+      rounds
+    };
+
+  }
+
+  async downloadSessionJSON(sessionId: string) {
+    const data = await this.getFullSessionData(sessionId);
+    return downloadObjectToJSON(sessionId, data);
   }
 
 
